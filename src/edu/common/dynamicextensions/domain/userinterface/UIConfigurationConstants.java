47f15ad3aba50c29d93521778463ba3acf566142
preeti_munot <preeti_munot>
2006-09-29 11:55:02 +0000

    Constants used in UI Configuration XML
    SVN-Revision: 19

---
+++ src/edu/common/dynamicextensions/domain/userinterface/UIConfigurationConstants.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Sep 27, 2006
+ * @author
+ *
+ */
+package edu.common.dynamicextensions.domain.userinterface;
+
+//Static constant definition class
+public class UIConfigurationConstants {
+	public static final String DISPLAYTYPE_CLASSNAME_MAPPING_FILE ="displaytype-classname-mapping";
+	//Attribute and tag names in the UI Configuration XML
+	public static final String UICONTROL_TAGNAME ="control";
+	public static final String REFERENCES_TAGNAME ="references";
+	public static final String PROPERTY_FILENAME_TAGNAME ="propertyfilename";
+	public static final String NAME_ATTRIBUTE = "name";
+	public static final String DISPLAY_TYPE_ATTRIBUTE = "display-type";
+	public static final String CAPTION_ATTRIBUTE = "caption";
+	public static final String DEFAULT_VALUE_ATTRIBUTE = "default-value";
+	public static final String CSS_CLASSNAME_ATTRIBUTE = "css-classname";
+	
+	public static final String VALUES_LIST= "list-of-values";
+	public static final String VALUE_SEPARATOR = "#";
+}

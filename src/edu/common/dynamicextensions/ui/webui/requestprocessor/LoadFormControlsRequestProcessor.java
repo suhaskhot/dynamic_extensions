e0986c7887dec4707c53b047131fbcc317a2be59
sumit_deshmukh <sumit_deshmukh>
2006-10-03 11:09:59 +0000

    Adding requestprocessor classes
    SVN-Revision: 37

---
+++ src/edu/common/dynamicextensions/ui/webui/requestprocessor/LoadFormControlsRequestProcessor.java
@@ -0,0 +1,13 @@
+package edu.common.dynamicextensions.ui.webui.requestprocessor;
+
+public class LoadFormControlsRequestProcessor {
+
+	public LoadFormControlsRequestProcessor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public void processRequest() {
+		
+	}
+
+}

18887cf6b2b0de3bf88c83c903dedb642ddf81e1
sujay_narkar <sujay_narkar>
2006-10-04 08:39:03 +0000

    Creation version
    SVN-Revision: 59

---
+++ src/edu/common/dynamicextensions/ui/webui/util/CacheManager.java
@@ -0,0 +1,12 @@
+
+package edu.common.dynamicextensions.ui.webui.util;
+
+/**
+ * @author sujay_narkar
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CacheManager {
+
+}

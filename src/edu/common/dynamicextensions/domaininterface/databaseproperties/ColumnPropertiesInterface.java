55e0b3388a0be0ba7ae227be18b40cc781aa2fd9
sujay_narkar <sujay_narkar>
2006-10-06 08:57:20 +0000

    creation version
    SVN-Revision: 87

---
+++ src/edu/common/dynamicextensions/domaininterface/databaseproperties/ColumnPropertiesInterface.java
@@ -0,0 +1,17 @@
+
+package edu.common.dynamicextensions.domaininterface.databaseproperties;
+
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.exception.AssignDataException;
+
+/**
+ * @author sujay_narkar
+ *
+ */
+public interface ColumnPropertiesInterface {
+     /**
+     * 
+     */
+    public void setAllValues(AbstractActionForm arg0) throws AssignDataException;
+
+}

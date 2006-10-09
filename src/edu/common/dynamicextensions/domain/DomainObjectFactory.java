76a574f2057ad46645ce39dd927a353eaed9dd73
geetika_bangard <geetika_bangard>
2006-10-09 11:33:44 +0000

    some methods added
    SVN-Revision: 116

--- src/edu/common/dynamicextensions/domain/DomainObjectFactory.java
+++ src/edu/common/dynamicextensions/domain/DomainObjectFactory.java
@@ -1,6 +1,22 @@
 
 package edu.common.dynamicextensions.domain;
 
+import edu.common.dynamicextensions.domain.databaseproperties.ColumnProperties;
+import edu.common.dynamicextensions.domain.databaseproperties.ConstraintProperties;
+import edu.common.dynamicextensions.domain.databaseproperties.TableProperties;
+import edu.common.dynamicextensions.domaininterface.AssociationInterface;
+import edu.common.dynamicextensions.domaininterface.BooleanAttributeInterface;
+import edu.common.dynamicextensions.domaininterface.ByteArrayAttributeInterface;
+import edu.common.dynamicextensions.domaininterface.DateAttributeInterface;
+import edu.common.dynamicextensions.domaininterface.EntityGroupInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.domaininterface.PrimitiveAttributeInterface;
+import edu.common.dynamicextensions.domaininterface.RoleInterface;
+import edu.common.dynamicextensions.domaininterface.SemanticPropertyInterface;
+import edu.common.dynamicextensions.domaininterface.databaseproperties.ColumnPropertiesInterface;
+import edu.common.dynamicextensions.domaininterface.databaseproperties.ConstraintPropertiesInterface;
+import edu.common.dynamicextensions.domaininterface.databaseproperties.TablePropertiesInterface;
+
 /**
  * @author sujay_narkar
  *
@@ -30,5 +46,99 @@ public class DomainObjectFactory {
 		}
 		return domainObjectFactory ;
 	}
+	
+	/**
+	 * Returns an object of Entity.
+	 * @return an instance of Entity.
+	 */
+	public EntityInterface createEntity(){
+	    Entity entity = new Entity();
+	    return entity;
+	}
+	
+	/**
+	 * 
+	 * @return an instance of EntityGroup.
+	 */
+	public EntityGroupInterface createEntityGroup(){
+	    EntityGroup entityGroup = new EntityGroup();
+	    return entityGroup;
+	}
+	
+	/**
+	 * 
+	 * @return an instance of Association.
+	 */
+	public AssociationInterface createAssociation(){
+	     Association association = new Association();
+	     return association;
+	}
+	/**
+	 * 
+	 * @return an instance of Role.
+	 */
+	public RoleInterface createRole(){
+	    Role role = new Role();
+	    return role;
+	}
+	
+	/**
+	 * 
+	 * @return an instance of SemanticProperty.
+	 */
+	public SemanticPropertyInterface createSemanticProperty() {
+	    SemanticProperty semanticProperty = new SemanticProperty();
+	    return semanticProperty;
+	}
+	
+	/**
+	 * 
+	 * @return an instance of ColumnProperties.
+	 */
+	public ColumnPropertiesInterface createColumnProperties(){
+	    ColumnProperties columnProperties = new ColumnProperties();
+	    return columnProperties;
+	}
+	/**
+	 * 
+	 * @return an instance of TableProperties.
+	 */
+	public TablePropertiesInterface createTableProperties(){
+	    TableProperties tableProperties = new TableProperties();
+	    return tableProperties;
+	}
+	/**
+	 * 
+	 * @return an instance of ConstraintProperties.
+	 */
+	public ConstraintPropertiesInterface createConstraintProperties(){
+	    ConstraintProperties constraintProperties = new ConstraintProperties();
+	    return constraintProperties;
+	}
+	/**
+	 * 
+	 * @return instance of BooleanAttribute.
+	 */
+	public BooleanAttributeInterface createBooleanAttribute(){
+	    BooleanAttribute booleanAttribute = new BooleanAttribute();
+	    return booleanAttribute;
+	}
+	/**
+	 * 
+	 * @return instance of ByteArrayAttribute.
+	 */
+	public ByteArrayAttributeInterface createByteArrayAttribute(){
+	    ByteArrayAttribute byteArrayAttribute = new ByteArrayAttribute();
+	    return byteArrayAttribute;
+	}
+	/**
+	 * 
+	 * @return instance of DateAttribute.
+	 */
+	public DateAttributeInterface createDateAttribute(){
+	    DateAttribute dateAttribute = new DateAttribute();
+	    return dateAttribute;
+	}
+	
 	
 }

757a1b997a0f8dc58784ea59ac5003d6418d142c
vishvesh_mulay <vishvesh_mulay>
2006-10-10 07:50:51 +0000

    Done with the most basic implementation.
    SVN-Revision: 129

---
+++ src/edu/common/dynamicextensions/processor/EntityProcessor.java
@@ -0,0 +1,98 @@
+/**
+ *<p>Title: EntityProcessor</p>
+ *<p>Description:  This class acts as a utility class which processes tne entity in various ways as needed
+ *and provides methods to the UI layer.This processor class is a POJO and not a framework specific class so 
+ *it can be used by all types of presentation layers.  </p>
+ *<p>Copyright:TODO</p>
+ *@author Vishvesh Mulay
+ *@version 1.0
+ */ 
+package edu.common.dynamicextensions.processor;
+
+import edu.common.dynamicextensions.domain.DomainObjectFactory;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.ui.interfaces.EntityInformationInterface;
+import edu.common.dynamicextensions.util.EntityManager;
+import edu.wustl.common.util.dbManager.DAOException;
+
+
+/**
+ *<p>Title: EntityProcessor</p>
+ *<p>Description:  This class acts as a utility class which processes tne entity in various ways as needed
+ *and provides methods to the UI layer.This processor class is a POJO and not a framework specific class so 
+ *it can be used by all types of presentation layers.  </p>
+ *@author Vishvesh Mulay
+ *@version 1.0
+ */ 
+public class EntityProcessor extends BaseDynamicExtensionsProcessor
+{
+
+    /**
+     * Protected constructor for entity processor
+     *
+     */
+   protected EntityProcessor () {
+       
+   }
+ 
+   /**
+    * this method gets the new instance of the entity processor to the caller.
+    * @return EntityProcessor EntityProcessor instance
+    */
+    public EntityProcessor getInstance () {
+        return new EntityProcessor();
+    }
+    
+    /**
+     * This method returns empty domain object of entityInterface.
+     * @return EntityInterface Returns new instance of EntityInterface from the domain object Factory.
+     */
+    public EntityInterface createEntity() {
+        return DomainObjectFactory.getInstance().createEntity();
+    }
+    
+    /**
+     * This method creates a new instance of the EntityInterface from the domain object factory. After the creation
+     * of this instance it populates the entityInterface with the information that is provided through 
+     * the entityInformationInterface which is a parameter to the method.
+     * @param entityInformationInterface Implementation of entityInformationInterface 
+     * which has all the data required for the creation of the entity.
+     * @return EntityInterface Returns the unsaved instance of EntityInterface with populated values taken 
+     * from the entityInformationInterface.
+     * @throws Exception Exception
+     */
+    public EntityInterface createAndSaveEntity(EntityInformationInterface entityInformationInterface) throws Exception {
+        EntityInterface entityInterface = DomainObjectFactory.getInstance().createEntity();
+        populateEntity(entityInformationInterface, entityInterface);
+        entityInterface = EntityManager.getInstance().createEntity(entityInterface);
+        return entityInterface;
+    }
+    
+    /**
+     * This method populates the given EntityInterface using the given entityInformationInterface.
+     * @param entityInterface Instance of EntityInterface which is populated using the informationInterface.
+     * @param entityInformationInterface Instance of EntityInformationInterface which is used to populate the entityInterface.
+     */
+    public void populateEntity (EntityInformationInterface entityInformationInterface, EntityInterface entityInterface) {
+        if (entityInformationInterface != null && entityInterface != null) {
+            entityInterface.setName(entityInformationInterface.getFormName());
+            entityInterface.setDescription(entityInformationInterface.getDescription());
+        }
+    }
+    
+    /**
+     * This method will populate the EntityInformationInterface using the EntityInterface so that the 
+     * information of the Entity can be shown on the user page using the EntityInformationInterface.
+     * @param entityInterface Instance of EntityInterface from which to populate the informationInterface.
+     * @param entityInformationInterface Instance of EntityInformationInterface which will be populated using 
+     * the first parameter that is EntityInterface.
+     */
+    public void populateEntityInformation (EntityInterface entityInterface, EntityInformationInterface entityInformationInterface) {
+        if (entityInterface != null && entityInformationInterface != null) {
+            entityInformationInterface.setFormName(entityInterface.getName());
+            entityInformationInterface.setDescription(entityInterface.getDescription());
+        }
+    }
+    
+    //public saveEntity(Enti)
+}

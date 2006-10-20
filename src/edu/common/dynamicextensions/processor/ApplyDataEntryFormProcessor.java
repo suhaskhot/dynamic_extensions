6b97483669b4e57461bafdcf39572ab9f9f054f3
chetan_patil <chetan_patil>
2006-10-20 08:59:47 +0000

    *** empty log message ***
    SVN-Revision: 329

---
+++ src/edu/common/dynamicextensions/processor/ApplyDataEntryFormProcessor.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package edu.common.dynamicextensions.processor;
+
+import java.util.Map;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
+import edu.common.dynamicextensions.entitymanager.EntityManager;
+import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+
+/**
+ * This Class populates the DataEntryForm and saves the same into the Database.
+ * @author chetan_patil
+ */
+public class ApplyDataEntryFormProcessor extends BaseDynamicExtensionsProcessor
+{
+
+	/**
+	 * Default Constructor
+	 */
+	public ApplyDataEntryFormProcessor()
+	{		
+	}
+	
+	/**
+	 * This method returns the instance of ApplyDataEntryFormProcessor.
+	 * @return ApplyDataEntryFormProcessor Instance of ApplyDataEntryFormProcessor
+	 */
+	public static ApplyDataEntryFormProcessor getInstance()
+	{
+		return new ApplyDataEntryFormProcessor();
+	}
+	
+	/**
+	 * This method will pass the values entered into the controls to EntityManager to insert them in Database.
+	 * @param containerInterface The container of who's value of Control are to be populated. 
+	 * @param attributeValueMap The Map of Attribute and their corresponding values from controls.
+	 * @throws DynamicExtensionsApplicationException on Application exception
+	 * @throws DynamicExtensionsSystemException on System exception
+	 */
+	public void insertDataEntryForm(ContainerInterface containerInterface, Map attributeValueMap) throws DynamicExtensionsApplicationException, DynamicExtensionsSystemException
+	{
+		try
+		{
+			EntityManager entityManager = EntityManager.getInstance();
+			
+			EntityInterface entityInterface = containerInterface.getEntity();
+			entityManager.insertData(entityInterface, attributeValueMap);
+		}
+		catch (DynamicExtensionsApplicationException e)
+		{
+			throw e;
+		}
+		catch (DynamicExtensionsSystemException e)
+		{
+			throw e;
+		}
+	}
+	
+}
--- src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
+++ src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
@@ -1,11 +1,63 @@
 
 package edu.common.dynamicextensions.ui.webui.action;
 
-/**
- * @author sujay_narkar
- *
- */
-public class ApplyDataEntryFormAction extends BaseDynamicExtensionsAction 
-{
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.common.dynamicextensions.domaininterface.AbstractAttributeInterface;
+import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
+import edu.common.dynamicextensions.domaininterface.userinterface.ControlInterface;
+import edu.common.dynamicextensions.ui.webui.util.CacheManager;
+import edu.common.dynamicextensions.util.global.Constants;
+
+/**
+ * This class is invoked on Apply click action.
+ * It populates the Attribute values entered in the dynamically generated controls. * 
+ * @author chetan_patil
+ */
+public class ApplyDataEntryFormAction extends BaseDynamicExtensionsAction
+{
+	/**
+	 * @param mapping ActionMapping
+	 * @param form ActionForm
+	 * @param request HttpServletRequest
+	 * @param response HttpServletResponse
+	 * @return ActionForward
+	 */
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+	{
+		ContainerInterface containerInterface = (ContainerInterface) CacheManager.getObjectFromCache(request, Constants.CONTAINER_INTERFACE);
+		Collection controlCollection = containerInterface.getControlCollection();
+		AbstractAttributeInterface abstractAttributeInterface = null;
+		ControlInterface controlInterface = null;
+		
+		Map attributeValueMap = new HashMap();		
+		String value = null;
+
+		for (int sequence = 1; sequence < controlCollection.size(); sequence++)
+		{
+			value = request.getParameter("control_" + sequence);
+
+			for (Iterator controlCollectionIterator = controlCollection.iterator(); controlCollectionIterator.hasNext();)
+			{
+				controlInterface = (ControlInterface) controlCollectionIterator.next();
+				if(sequence == controlInterface.getSequenceNumber().intValue())
+				{
+					abstractAttributeInterface = controlInterface.getAbstractAttribute();
+					attributeValueMap.put(abstractAttributeInterface, value);
+					break;
+				}
+			}
+		}
+		return (mapping.findForward(Constants.SUCCESS));
+	}
 }

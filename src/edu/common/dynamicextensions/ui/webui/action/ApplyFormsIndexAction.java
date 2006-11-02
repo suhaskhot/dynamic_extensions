1baba8821c2cded99d661c0292a1322af7a5256e
chetan_patil <chetan_patil>
2006-11-02 12:24:40 +0000

    *** empty log message ***
    SVN-Revision: 425

--- src/edu/common/dynamicextensions/ui/webui/action/ApplyFormsIndexAction.java
+++ src/edu/common/dynamicextensions/ui/webui/action/ApplyFormsIndexAction.java
@@ -37,6 +37,7 @@ public class ApplyFormsIndexAction extends BaseDynamicExtensionsAction
 			actionForward = mapping.findForward(Constants.INSERT_DATA);
 		}
 		
-		return  mapping.findForward(Constants.SUCCESS);
+		//return mapping.findForward(Constants.SUCCESS);
+		return actionForward;
 	}
 }

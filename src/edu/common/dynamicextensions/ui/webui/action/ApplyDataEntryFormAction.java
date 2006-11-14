7ce59d83ada683fbaf5d1cd1a5969045c995d79f
sujay_narkar <sujay_narkar>
2006-11-14 06:48:14 +0000

    Made changes for web ui manager
    SVN-Revision: 521

--- src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
+++ src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
@@ -21,6 +21,7 @@ import edu.common.dynamicextensions.domaininterface.userinterface.ControlInterfa
 import edu.common.dynamicextensions.processor.ApplyDataEntryFormProcessor;
 import edu.common.dynamicextensions.ui.webui.util.CacheManager;
 import edu.common.dynamicextensions.ui.webui.util.WebUIManager;
+import edu.common.dynamicextensions.ui.webui.util.WebUIManagerConstants;
 import edu.common.dynamicextensions.util.global.Constants;
 import edu.common.dynamicextensions.validation.ValidatorUtil;
 
@@ -40,53 +41,14 @@ public class ApplyDataEntryFormAction extends BaseDynamicExtensionsAction
 	 */
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
 	{
-		try 
-		{
-			Long recordIdentifier = new Long(-1);
-			ContainerInterface containerInterface = (ContainerInterface) CacheManager.getObjectFromCache(request, Constants.CONTAINER_INTERFACE);
-			Collection<ControlInterface> controlCollection = containerInterface.getControlCollection();
-			
-			Map<AbstractAttributeInterface, String> attributeValueMap = getAttributeValuesFromRequest(controlCollection,request);
-			List<String> errorList = ValidatorUtil.validateEntity(attributeValueMap);
-			
-			if (errorList.size() != 0)
-			{
-				saveMessages(request, getErrorMessages(errorList));
-			}
-			else
-			{
-				ApplyDataEntryFormProcessor applyDataEntryFormProcessor = ApplyDataEntryFormProcessor.getInstance();
-				recordIdentifier = applyDataEntryFormProcessor.insertDataEntryForm(containerInterface, attributeValueMap);
-				saveMessages(request, getSuccessMessage());
-			}
+		ContainerInterface containerInterface = (ContainerInterface) CacheManager.getObjectFromCache(request, Constants.CONTAINER_INTERFACE);
+		Collection<ControlInterface> controlCollection = containerInterface.getControlCollection();
+		AbstractAttributeInterface abstractAttributeInterface = null;
+		//ControlInterface controlInterface = null;
 
-			String calllbackURL = (String) CacheManager.getObjectFromCache(request, Constants.CALLBACK_URL);
-			if (calllbackURL != null && !calllbackURL.equals(""))
-			{
-				calllbackURL = calllbackURL + "?" + WebUIManager.getRecordIdentifierParameterName() + "=" + recordIdentifier ;
-				CacheManager.clearCache(request);
-				response.sendRedirect(calllbackURL);
-				return null;
-			}
-			return (mapping.findForward(Constants.SUCCESS));
-		}
-		catch (Exception e)
-		{
-			String actionForwardString = catchException(e, request);
-			return (mapping.findForward(actionForwardString));
-		}
-	}
-
-	/**
-	 * @param request 
-	 * @return
-	 */
-	private Map<AbstractAttributeInterface, String> getAttributeValuesFromRequest(Collection<ControlInterface> controlCollection, HttpServletRequest request)
-	{
 		Map<AbstractAttributeInterface, String> attributeValueMap = new LinkedHashMap<AbstractAttributeInterface, String>();
 		String value = null;
-		//Collection<ControlInterface> controlCollection = containerInterface.getControlCollection();
-		AbstractAttributeInterface abstractAttributeInterface = null;
+
 		for (int sequence = 1; sequence <= controlCollection.size(); sequence++)
 		{
 			value = request.getParameter("Control_" + sequence);
@@ -100,7 +62,41 @@ public class ApplyDataEntryFormAction extends BaseDynamicExtensionsAction
 				}
 			}
 		}
-		return attributeValueMap;
+
+		ApplyDataEntryFormProcessor applyDataEntryFormProcessor = ApplyDataEntryFormProcessor.getInstance();
+
+		List<String> errorList;
+		try
+		{
+			errorList = ValidatorUtil.validateEntity(attributeValueMap);
+			Long recordIdentifier = new Long(-1);
+
+			if (errorList.size() != 0)
+			{
+				saveMessages(request, getErrorMessages(errorList));
+			}
+			else
+			{
+				recordIdentifier = applyDataEntryFormProcessor.insertDataEntryForm(containerInterface, attributeValueMap);
+				saveMessages(request, getSuccessMessage());
+			}
+
+			String calllbackURL = (String) CacheManager.getObjectFromCache(request, Constants.CALLBACK_URL);
+			if (calllbackURL != null && !calllbackURL.equals(""))
+			{
+				calllbackURL = calllbackURL + "?" + WebUIManager.getRecordIdentifierParameterName() + "=" + recordIdentifier + "&"
+						+ WebUIManager.getOperationStatusParameterName() + "=" + WebUIManagerConstants.SUCCESS;
+				CacheManager.clearCache(request);
+				response.sendRedirect(calllbackURL);
+				return null;
+			}
+		}
+		catch (Exception e)
+		{
+			String actionForwardString = catchException(e, request);
+			return (mapping.findForward(actionForwardString));
+		}
+		return (mapping.findForward(Constants.SUCCESS));
 	}
 
 	/**
@@ -114,4 +110,13 @@ public class ApplyDataEntryFormAction extends BaseDynamicExtensionsAction
 		return actionMessages;
 	}
 
+	/*public ActionErrors getErrorMessages(List<String> errorList)
+	 {
+	 ActionErrors errors = new ActionErrors();
+	 for (String errorMessage : errorList)
+	 {
+	 errors.add(ActionErrors.GLOBAL_ERROR, new ActionError(errorMessage));
+	 }
+	 return errors;
+	 }*/
 }

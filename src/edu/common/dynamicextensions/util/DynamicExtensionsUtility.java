38bfa29b182cde6dd792792569edd81bbf2b86a6
preeti_munot <preeti_munot>
2006-11-30 04:30:05 +0000

    *** empty log message ***
    SVN-Revision: 711

--- src/edu/common/dynamicextensions/util/DynamicExtensionsUtility.java
+++ src/edu/common/dynamicextensions/util/DynamicExtensionsUtility.java
@@ -12,7 +12,6 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;

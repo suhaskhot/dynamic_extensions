ad9a6a80377595bf009b1acff62f65123c999f49
vishvesh_mulay <vishvesh_mulay>
2006-12-06 07:28:38 +0000

    *** empty log message ***
    SVN-Revision: 768

--- src/edu/common/dynamicextensions/entitymanager/EntityManagerInterface.java
+++ src/edu/common/dynamicextensions/entitymanager/EntityManagerInterface.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import edu.common.dynamicextensions.domain.Entity;
 import edu.common.dynamicextensions.domain.userinterface.SelectControl;
 import edu.common.dynamicextensions.domaininterface.AbstractAttributeInterface;
 import edu.common.dynamicextensions.domaininterface.AssociationInterface;
@@ -58,7 +59,6 @@ public interface EntityManagerInterface
 	 */
 	EntityGroupInterface persistEntityGroup(EntityGroupInterface entityGroupInterface) throws DynamicExtensionsSystemException,
 			DynamicExtensionsApplicationException;
-
 	/**
 	 * This method returns the EntityGroupInterface given the short name for the 
 	 * entity.
@@ -67,7 +67,7 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsSystemException
 	 */
 	EntityGroupInterface getEntityGroupByShortName(String entityGroupShortName) throws DynamicExtensionsSystemException;
-
+	
 	/**
 	 * Returns a collection of association objects given the source entity id and
 	 * target entity id.
@@ -88,7 +88,7 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsApplicationException
 	 */
 	EntityInterface getEntityByName(String entityName) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
-
+	
 	/**
 	 * Returns an attribute given the entity name and attribute name.
 	 * @param entityName
@@ -99,7 +99,7 @@ public interface EntityManagerInterface
 	 */
 	AttributeInterface getAttribute(String entityName, String attributeName) throws DynamicExtensionsSystemException,
 			DynamicExtensionsApplicationException;
-
+	
 	/**
 	 * Returns an association object given the entity name and source role name.
 	 * @param entityName
@@ -110,7 +110,7 @@ public interface EntityManagerInterface
 	 */
 	AssociationInterface getAssociation(String entityName, String sourceRoleName) throws DynamicExtensionsSystemException,
 			DynamicExtensionsApplicationException;
-
+	
 	/**
 	 * Returns a collection of entities given the entity concept code.
 	 * @param entityConceptCode
@@ -118,25 +118,27 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityInterface> getEntitiesByConceptCode(String entityConceptCode) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
-
+	Collection<EntityInterface> getEntitiesByConceptCode(String entityConceptCode) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+		
+	
 	/**
 	 * Returns all entities in the whole system
 	 * @return
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityInterface> getAllEntities() throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
-
+	Collection<EntityInterface>  getAllEntities() throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+	
+	
 	/**
 	 * Returns a single  entity for given identifier
 	 * @return
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	public EntityInterface getEntityByIdentifier(String identifier) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
-
+	public EntityInterface getEntityByIdentifier(String identifier) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;	
+	
+	
 	/**
 	 * Returns a collection of entities having attribute with the given name  
 	 * @param attributeName
@@ -144,9 +146,8 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityInterface> getEntitiesByAttributeName(String attributeName) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
-
+	Collection<EntityInterface> getEntitiesByAttributeName(String attributeName) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+	
 	/**
 	 * Returns all Containers in the whole system
 	 * @return Collection of ContainerInterface
@@ -162,8 +163,7 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityInterface> getEntityByDescription(String entityDescription) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
+	Collection<EntityInterface> getEntityByDescription(String entityDescription) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
 
 	/**
 	 * Returns a collection of Entity objects given the attribute description
@@ -174,7 +174,6 @@ public interface EntityManagerInterface
 	 */
 	Collection<EntityInterface> getEntitiesByAttributeDescription(String attributeDescription) throws DynamicExtensionsSystemException,
 			DynamicExtensionsApplicationException;
-
 	/**
 	 * Returns a collection of entity objects given the entity concept name.
 	 * @param entityConceptName
@@ -182,8 +181,7 @@ public interface EntityManagerInterface
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityInterface> getEntitiesByConceptName(String entityConceptName) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
+	Collection<EntityInterface> getEntitiesByConceptName(String entityConceptName) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
 
 	/**
 	 * Returns a collection of entities given attribute concept code. 
@@ -226,8 +224,7 @@ public interface EntityManagerInterface
 	/**
 	 * This method inserts one record for the entity.
 	 */
-	Long insertData(EntityInterface entity, Map<AbstractAttributeInterface, Object> dataValue) throws DynamicExtensionsApplicationException,
-			DynamicExtensionsSystemException;
+	Long insertData(EntityInterface entity, Map<AbstractAttributeInterface, Object> dataValue) throws DynamicExtensionsApplicationException, DynamicExtensionsSystemException;
 
 	/**
 	 * Returns a particular record for the given recordId of the given entityId
@@ -236,50 +233,55 @@ public interface EntityManagerInterface
 	 * @return Map key - attribute name 
 	 *             value - attribute value  
 	 */
-	Map<String, Object> getRecordById(EntityInterface entity, Long recordId) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
+	Map<String, Object> getRecordById(EntityInterface entity, Long recordId) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
 
-	/**
-	 * This method deletes a particular record for an entity.
-	 * @param entity Entity for which record needs to be deleted
-	 * @param recordId Id of the record
-	 * @return success failure flag. true if successful
-	 * @throws DynamicExtensionsApplicationException
-	 * @throws DynamicExtensionsSystemException
-	 */
-	boolean deleteRecord(EntityInterface entity, Long recordId) throws DynamicExtensionsApplicationException, DynamicExtensionsSystemException;
-
-	/**
-	 * This method updates the existing record for the given entity.
-	 * @param entity       Entity for which record needs to be updated
-	 * @param dataValue    map that contains  name of the attribute whose value is changed and its new value
-	 *                     If it is multiselect attribute then valu should be List<string>
-	 * @param recordId     Id of the record
-	 * @return true if success
-	 * @throws DynamicExtensionsApplicationException
-	 * @throws DynamicExtensionsSystemException
-	 */
-	boolean editData(EntityInterface entity, Map<AbstractAttributeInterface, ?> dataValue, Long recordId)
-			throws DynamicExtensionsApplicationException, DynamicExtensionsSystemException;
-
-	/**
+    /**
+     * This method deletes a particular record for an entity.
+     * @param entity Entity for which record needs to be deleted
+     * @param recordId Id of the record
+     * @return success failure flag. true if successful
+     * @throws DynamicExtensionsApplicationException
+     * @throws DynamicExtensionsSystemException
+     */
+    boolean deleteRecord (EntityInterface entity, Long recordId) throws DynamicExtensionsApplicationException,
+    DynamicExtensionsSystemException;
+    
+    /**
+     * This method updates the existing record for the given entity.
+     * @param entity       Entity for which record needs to be updated
+     * @param dataValue    map that contains  name of the attribute whose value is changed and its new value
+     *                     If it is multiselect attribute then valu should be List<string>
+     * @param recordId     Id of the record
+     * @return true if success
+     * @throws DynamicExtensionsApplicationException
+     * @throws DynamicExtensionsSystemException
+     */
+    boolean editData(EntityInterface entity, Map<AbstractAttributeInterface, ?> dataValue, Long recordId)
+            throws DynamicExtensionsApplicationException, DynamicExtensionsSystemException;
+   
+    /**
 	 * Returns all entityGroups in the system.
 	 * @return
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	Collection<EntityGroupInterface> getAllEntitiyGroups() throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+	Collection<EntityGroupInterface> getAllEntitiyGroups() throws DynamicExtensionsSystemException,	DynamicExtensionsApplicationException;
 
-	/**
+    /**
 	 * This method returns collection of all the containers of the entity contained within a 
 	 * entity group.
 	 * 
 	 * @param entityGroupIdentifier id of entity group 
 	 * @return collection of the containers
 	 */
-	Collection<ContainerInterface> getAllContainersByEntityGroupId(Long entityGroupIdentifier) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
+	Collection<ContainerInterface> getAllContainersByEntityGroupId(Long entityGroupIdentifier)
+	throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
 
-	Map<Long, List<String>> getTargetEntityDisplayAttribute(SelectControl selectControl) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException;
+	/**
+	 * This method is used to save the metadata information of the given entity without creating it's data 
+	 * table.
+	 */
+	EntityInterface persistEntityMetadata(EntityInterface entityInterface)
+			throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+	
 }

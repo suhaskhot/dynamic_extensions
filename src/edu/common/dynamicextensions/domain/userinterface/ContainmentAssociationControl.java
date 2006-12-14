3f631ce36a0399f486d9906c596fcd4e1a17a882
chetan_patil <chetan_patil>
2006-12-14 08:23:16 +0000

    Added method isCardinalityOneToMany()
    SVN-Revision: 853

--- src/edu/common/dynamicextensions/domain/userinterface/ContainmentAssociationControl.java
+++ src/edu/common/dynamicextensions/domain/userinterface/ContainmentAssociationControl.java
@@ -8,10 +8,13 @@
 
 package edu.common.dynamicextensions.domain.userinterface;
 
+import edu.common.dynamicextensions.domaininterface.AssociationInterface;
+import edu.common.dynamicextensions.domaininterface.RoleInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.ContainmentAssociationControlInterface;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.common.dynamicextensions.ui.webui.util.UserInterfaceiUtility;
+import edu.common.dynamicextensions.util.global.Constants.Cardinality;
 
 /**
  * @author vishvesh_mulay
@@ -23,8 +26,9 @@ public class ContainmentAssociationControl extends Control implements Containmen
 
 	/**
 	 * 
-	 *//*
-	private static final long serialVersionUID = -7680442136446012194L;*/
+	 */
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * 
 	 */
@@ -61,5 +65,21 @@ public class ContainmentAssociationControl extends Control implements Containmen
 	public String generateHTML() throws DynamicExtensionsSystemException
 	{
 		return UserInterfaceiUtility.generateHTML(container);
+	}
+	
+	/**
+	 * This method returns true if the cardinality of the Containment Association is One to Many.
+	 * @return true if Caridnality is One to Many, false otherwise.
+	 */
+	public boolean isCardinalityOneToMany()
+	{
+		boolean isOneToMany = false;
+		AssociationInterface associationInterface = (AssociationInterface)this.getAbstractAttribute();
+		RoleInterface targetRole = associationInterface.getTargetRole();
+		if (targetRole.getMaximumCardinality() == Cardinality.MANY)
+		{
+			isOneToMany = true;
+		}
+		return isOneToMany;
 	}
 }

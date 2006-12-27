051db10f6f743afcb792c94448b91aacc95ae432
sujay_narkar <sujay_narkar>
2006-12-27 10:48:15 +0000

    *** empty log message ***
    SVN-Revision: 985

--- src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
+++ src/edu/common/dynamicextensions/ui/webui/action/ApplyDataEntryFormAction.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
-import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;

04ca16e6eac8f8977dc8410ad32def580fe9a83c
Rahul_Ner <Rahul_Ner>
2007-01-04 12:50:05 +0000

    *** empty log message ***
    SVN-Revision: 1039

--- src/edu/common/dynamicextensions/domain/AbstractMetadata.java
+++ src/edu/common/dynamicextensions/domain/AbstractMetadata.java
@@ -99,7 +99,7 @@ public abstract class AbstractMetadata extends DynamicExtensionBaseDomainObject
 
 	/**
 	 * This method returns the description of the AbstractMetadata.
-	 * @hibernate.property name="description" type="string" column="DESCRIPTION" length="1000"
+	 * @hibernate.property name="description" type="string" column="DESCRIPTION" 
 	 * @return the description of the AbstractMetadata.
 	 */
 	public String getDescription()
@@ -137,7 +137,7 @@ public abstract class AbstractMetadata extends DynamicExtensionBaseDomainObject
 
 	/**
 	 * This method returns the name of the AbstractMetadata.
-	 * @hibernate.property name="name" type="string" column="NAME" length = "40"
+	 * @hibernate.property name="name" type="string" column="NAME" 
 	 * @return the name of the AbstractMetadata.
 	 */
 	public String getName()

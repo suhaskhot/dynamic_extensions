a2f424f31afcebc25c79e53086f7ff03cd80088c
Rahul_Ner <Rahul_Ner>
2007-01-15 12:49:14 +0000

    *** empty log message ***
    SVN-Revision: 1100

--- src/edu/common/dynamicextensions/entitymanager/DynamicExtensionMySQLQueryBuilder.java
+++ src/edu/common/dynamicextensions/entitymanager/DynamicExtensionMySQLQueryBuilder.java
@@ -1,6 +1,8 @@
 
 package edu.common.dynamicextensions.entitymanager;
 
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+
 /**
  * This class provides the methods that builds the queries that are specific to ORACLE database 
  * 
@@ -9,5 +11,25 @@ package edu.common.dynamicextensions.entitymanager;
 
 public class DynamicExtensionMySQLQueryBuilder extends DynamicExtensionBaseQueryBuilder
 {
+	
+	/**
+	 * This method returns the query to add foreign key constraint in the given child entity
+	 * that refers to identifier column of the parent.
+	 * @param entity
+	 * @return
+	 */
+	protected String getForeignKeyRemoveConstraintQueryForInheritance(EntityInterface entity)
+	{
+		StringBuffer foreignKeyConstraint = new StringBuffer();
+		EntityInterface parentEntity = entity.getParentEntity();
+		String foreignConstraintName = "FK" + "E" + entity.getId() + "E" + parentEntity.getId();
+
+		foreignKeyConstraint.append(ALTER_TABLE).append(WHITESPACE).append(
+				entity.getTableProperties().getName()).append(WHITESPACE).append(DROP_KEYWORD)
+				.append(WHITESPACE).append(FOREIGN_KEY_KEYWORD).append(WHITESPACE).append(
+						foreignConstraintName);
+
+		return foreignKeyConstraint.toString();
+	}
 
 }

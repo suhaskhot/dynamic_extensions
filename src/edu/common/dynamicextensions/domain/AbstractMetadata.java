a92f1b03b4f70da8e9eb5b5677a50f095514b007
Rahul_Ner <Rahul_Ner>
2007-02-23 09:22:56 +0000

    *** empty log message ***
    SVN-Revision: 1260

--- src/edu/common/dynamicextensions/domain/AbstractMetadata.java
+++ src/edu/common/dynamicextensions/domain/AbstractMetadata.java
@@ -1,11 +1,15 @@
 
 package edu.common.dynamicextensions.domain;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 
 import edu.common.dynamicextensions.domaininterface.AbstractMetadataInterface;
+import edu.common.dynamicextensions.domaininterface.SemanticPropertyInterface;
 import edu.common.dynamicextensions.domaininterface.TaggedValueInterface;
 
 /**
@@ -16,10 +20,10 @@ import edu.common.dynamicextensions.domaininterface.TaggedValueInterface;
  * @hibernate.class table="DYEXTN_ABSTRACT_METADATA" 
  * @hibernate.cache  usage="read-write"
  */
-public abstract class AbstractMetadata extends SemanticAnnotatable
+public abstract class AbstractMetadata extends DynamicExtensionBaseDomainObject
 		implements
-			AbstractMetadataInterface,
-			SemanticAnnotatableInterface
+			AbstractMetadataInterface
+			
 {
 
 	/**
@@ -63,6 +67,14 @@ public abstract class AbstractMetadata extends SemanticAnnotatable
 	 * Semantic property collection.
 	 */
 	protected Collection<TaggedValueInterface> taggedValueCollection = new HashSet<TaggedValueInterface>();
+	
+	/**
+	 * Semantic property collection.
+	 */
+	protected Collection<SemanticPropertyInterface> semanticPropertyCollection = new HashSet<SemanticPropertyInterface>();
+
+	
+	
 
 	/**
 	 * Empty Constructor
@@ -187,6 +199,81 @@ public abstract class AbstractMetadata extends SemanticAnnotatable
 		}
 		taggedValueCollection.add(taggedValueInterface);
 
+	}
+	
+	/**
+	 * This method sets the semanticPropertyCollection to the given Collection of SemanticProperties.
+	 * @param semanticPropertyCollection the Collection of SemanticProperties to be set.
+	 */
+	public void setSemanticPropertyCollection(
+			Collection<SemanticPropertyInterface> semanticPropertyCollection)
+	{
+		this.semanticPropertyCollection = semanticPropertyCollection;
+	}
+
+	/**
+	 * This method returns the Collection of SemanticProperties of the AbstractMetadata.
+	 * @hibernate.set name="semanticPropertyCollection" cascade="save-update"
+	 * inverse="false" lazy="false"
+	 * @hibernate.collection-key column="ABSTRACT_METADATA_ID"
+	 * @hibernate.collection-one-to-many class="edu.common.dynamicextensions.domain.SemanticProperty"
+	 * @hibernate.cache  usage="read-write"
+	 * @return the Collection of SemanticProperties of the AbstractMetadata.
+	 */
+	public Collection<SemanticPropertyInterface> getSemanticPropertyCollection()
+	{
+		return semanticPropertyCollection;
+	}
+	/**
+	 * This method adds a SemanticProperty to the AbstractMetadata.
+	 * @param semanticPropertyInterface A SemanticProperty to be added.
+	 */
+	public void addSemanticProperty(SemanticPropertyInterface semanticPropertyInterface)
+	{
+		if (semanticPropertyCollection == null)
+		{
+			semanticPropertyCollection = new HashSet<SemanticPropertyInterface>();
+		}
+		semanticPropertyCollection.add(semanticPropertyInterface);
+	}
+
+	/**
+	 * This method removes a SemanticProperty from the AbstractMetadata.
+	 * @param semanticPropertyInterface A SemanticProperty to be removed.
+	 */
+	public void removeSemanticProperty(SemanticPropertyInterface semanticPropertyInterface)
+	{
+		if ((semanticPropertyCollection != null)
+				&& (semanticPropertyCollection.contains(semanticPropertyInterface)))
+		{
+			semanticPropertyCollection.remove(semanticPropertyInterface);
+		}
+	}
+
+	/**
+	 * This method removes all SemanticProperties from AbstractMetadata.
+	 */
+	public void removeAllSemanticProperties()
+	{
+		if (semanticPropertyCollection != null)
+		{
+			semanticPropertyCollection.clear();
+		}
+	}
+
+	/**
+	 * @see edu.common.dynamicextensions.domaininterface.AbstractMetadataInterface#getOrderedSemanticPropertyCollection()
+	 */
+	public List<SemanticPropertyInterface> getOrderedSemanticPropertyCollection()
+	{
+		List<SemanticPropertyInterface> semanticPropertyList = new ArrayList<SemanticPropertyInterface>();
+
+		if (this.semanticPropertyCollection != null && !this.semanticPropertyCollection.isEmpty())
+		{
+			semanticPropertyList.addAll(this.semanticPropertyCollection);
+			Collections.sort(semanticPropertyList);
+		}
+		return semanticPropertyList;
 	}
 
 	/**
--- src/edu/common/dynamicextensions/domain/PermissibleValue.java
+++ src/edu/common/dynamicextensions/domain/PermissibleValue.java
@@ -1,22 +1,35 @@
 
 package edu.common.dynamicextensions.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 import edu.common.dynamicextensions.domaininterface.PermissibleValueInterface;
+import edu.common.dynamicextensions.domaininterface.SemanticPropertyInterface;
+import edu.wustl.common.domain.AbstractDomainObject;
 
 /**
  * @author sujay_narkar
  * @hibernate.class table="DYEXTN_PERMISSIBLE_VALUE" 
  *
  */
-public abstract class PermissibleValue extends SemanticAnnotatable
-		implements
-			PermissibleValueInterface
+public abstract class PermissibleValue extends DynamicExtensionBaseDomainObject
+		implements PermissibleValueInterface
 {
 
 	/**
 	 * 
 	 */
 	protected String description;
+
+	/**
+	 * Semantic property collection.
+	 */
+
+	protected Collection<SemanticPropertyInterface> semanticPropertyCollection = new HashSet<SemanticPropertyInterface>();
 
 	/**
 	 * @hibernate.property name="description" type="string" column="DESCRIPTION" 
@@ -36,6 +49,7 @@ public abstract class PermissibleValue extends SemanticAnnotatable
 		this.description = description;
 	}
 
+
 	/**
 	 * @see edu.common.dynamicextensions.domaininterface.PermissibleValueInterface#getValueAsObject()
 	 */
@@ -51,4 +65,77 @@ public abstract class PermissibleValue extends SemanticAnnotatable
 	{
 		return id;
 	}
+
+	/**
+	 * @hibernate.set name="semanticPropertyCollection" cascade="save-update"
+	 * inverse="false" lazy="false"
+	 * @hibernate.collection-key column="ABSTRACT_VALUE_ID"
+	 * @hibernate.collection-one-to-many class="edu.common.dynamicextensions.domain.SemanticProperty"
+	 * @return Returns the semanticPropertyCollection.
+	 */
+	public Collection getSemanticPropertyCollection()
+	{
+		return semanticPropertyCollection;
+	}
+
+	/**
+	 * @param semanticPropertyCollection The semanticPropertyCollection to set.
+	 */
+	public void setSemanticPropertyCollection(Collection semanticPropertyCollection)
+	{
+		this.semanticPropertyCollection = semanticPropertyCollection;
+	}
+	
+	/**
+	 * This method adds a SemanticProperty to the AbstractMetadata.
+	 * @param semanticPropertyInterface A SemanticProperty to be added.
+	 */
+	public void addSemanticProperty(SemanticPropertyInterface semanticPropertyInterface)
+	{
+		if (semanticPropertyCollection == null)
+		{
+			semanticPropertyCollection = new HashSet<SemanticPropertyInterface>();
+		}
+		semanticPropertyCollection.add(semanticPropertyInterface);
+	}
+
+	/**
+	 * This method removes a SemanticProperty from the AbstractMetadata.
+	 * @param semanticPropertyInterface A SemanticProperty to be removed.
+	 */
+	public void removeSemanticProperty(SemanticPropertyInterface semanticPropertyInterface)
+	{
+		if ((semanticPropertyCollection != null)
+				&& (semanticPropertyCollection.contains(semanticPropertyInterface)))
+		{
+			semanticPropertyCollection.remove(semanticPropertyInterface);
+		}
+	}
+
+	/**
+	 * This method removes all SemanticProperties from AbstractMetadata.
+	 */
+	public void removeAllSemanticProperties()
+	{
+		if (semanticPropertyCollection != null)
+		{
+			semanticPropertyCollection.clear();
+		}
+	}
+
+	/**
+	 * @see edu.common.dynamicextensions.domaininterface.AbstractMetadataInterface#getOrderedSemanticPropertyCollection()
+	 */
+	public List<SemanticPropertyInterface> getOrderedSemanticPropertyCollection()
+	{
+		List<SemanticPropertyInterface> semanticPropertyList = new ArrayList<SemanticPropertyInterface>();
+
+		if (this.semanticPropertyCollection != null && !this.semanticPropertyCollection.isEmpty())
+		{
+			semanticPropertyList.addAll(this.semanticPropertyCollection);
+			Collections.sort(semanticPropertyList);
+		}
+		return semanticPropertyList;
+	}
+
 }

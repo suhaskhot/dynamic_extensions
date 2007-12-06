8bccfd66c4aa3c83a2efaf66828568bb84f726d5
kunal_kamble <kunal_kamble>
2007-12-06 15:00:46 +0000

    --IdGenerator interface
    SVN-Revision: 4288

---
+++ src/edu/common/dynamicextensions/domaininterface/IdGeneratorInterface.java
@@ -0,0 +1,19 @@
+package edu.common.dynamicextensions.domaininterface;
+
+/**
+ * @author kunal_kamble
+ *
+ */
+public interface IdGeneratorInterface extends DynamicExtensionBaseDomainObjectInterface{
+	
+	/**
+	 * @return the next available unique id 
+	 */
+	public Long getNextAvailableId();
+	
+	/**
+	 * @param nextAvailableId 
+	 */
+	public void setNextAvailableId(Long nextAvailableId);
+
+}

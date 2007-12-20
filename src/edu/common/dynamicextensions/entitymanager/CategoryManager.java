da14008f8e1b91dbe276cbcbbc7bd53d8738a39c
mandar_shidhore <mandar_shidhore>
2007-12-20 09:08:42 +0000

    -- category related changes
    SVN-Revision: 4415

--- src/edu/common/dynamicextensions/entitymanager/CategoryManager.java
+++ src/edu/common/dynamicextensions/entitymanager/CategoryManager.java
@@ -3,10 +3,15 @@ package edu.common.dynamicextensions.entitymanager;
 import java.util.List;
 import java.util.Stack;
 
+import edu.common.dynamicextensions.domain.Category;
 import edu.common.dynamicextensions.domaininterface.AbstractMetadataInterface;
+import edu.common.dynamicextensions.domaininterface.CategoryInterface;
 import edu.common.dynamicextensions.domaininterface.DynamicExtensionBaseDomainObjectInterface;
 import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+import edu.common.dynamicextensions.util.DynamicExtensionsUtility;
+import edu.common.dynamicextensions.util.global.Constants;
+import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.HibernateDAO;
 
 /**
@@ -16,6 +21,38 @@ import edu.wustl.common.dao.HibernateDAO;
 */
 public class CategoryManager extends AbstractMetadataManager implements CategoryManagerInterface
 {
+    /**
+     * Static instance of the CategoryManager.
+     */
+    private static CategoryManagerInterface categoryManager = null;
+    
+    /**
+     * Static instance of the queryBuilder.
+     */
+    private static DynamicExtensionBaseQueryBuilder queryBuilder = null;
+    
+    /**
+     * Empty Constructor.
+     */
+    protected CategoryManager()
+    {
+        
+    }
+
+    /**
+     * Returns the instance of the Entity Manager.
+     * @return entityManager singleton instance of the Entity Manager.
+     */
+    public static synchronized CategoryManagerInterface getInstance()
+    {
+        if (categoryManager == null)
+        {
+            categoryManager = new CategoryManager();
+            DynamicExtensionsUtility.initialiseApplicationVariables();
+            queryBuilder = QueryBuilderFactory.getQueryBuilder();
+        }
+        return categoryManager;
+    }
 
 	/**
 	 * preProcess.
@@ -23,7 +60,6 @@ public class CategoryManager extends AbstractMetadataManager implements Category
 	protected void preProcess(DynamicExtensionBaseDomainObjectInterface dynamicExtensionBaseDomainObject, List reverseQueryList, HibernateDAO hibernateDAO, List queryList) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
 	{
 		// TODO Auto-generated method stub
-
 	}
 	/**
 	 * postProcess.
@@ -31,7 +67,6 @@ public class CategoryManager extends AbstractMetadataManager implements Category
 	protected void postProcess(List queryList, List reverseQueryList, Stack rollbackQueryStack) throws DynamicExtensionsSystemException
 	{
 		// TODO Auto-generated method stub
-
 	}
 	/**
 	 * LogFatalError.
@@ -39,6 +74,104 @@ public class CategoryManager extends AbstractMetadataManager implements Category
 	protected void LogFatalError(Exception e, AbstractMetadataInterface abstractMetadata)
 	{
 		// TODO Auto-generated method stub
-
 	}
+    
+    public CategoryInterface persistCategory(CategoryInterface categoryInterface) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+    {
+        logDebug("persistCategory", "entering the method");
+        Category category = (Category) categoryInterface;
+        boolean isCategorySaved = true;
+        if (category.getId() == null)
+        {
+            isCategorySaved = false;
+        }
+        
+        HibernateDAO hibernateDAO = (HibernateDAO) DAOFactory.getInstance().getDAO(
+                Constants.HIBERNATE_DAO);
+        Stack stack = new Stack();
+        
+        try
+        {
+            hibernateDAO.openSession(null);
+            
+            if (!isCategorySaved)
+                hibernateDAO.insert(category, null, false, false);
+            else
+                hibernateDAO.update(category, null, false, false, false);
+        
+            hibernateDAO.commit();
+        }
+        catch (Exception e)
+        {
+            rollbackQueries(stack, category, e, hibernateDAO);
+        
+            if (e instanceof DynamicExtensionsApplicationException)
+            {
+                throw (DynamicExtensionsApplicationException) e;
+            }
+            else
+            {
+                throw new DynamicExtensionsSystemException(e.getMessage(), e);
+            }
+        }
+        finally
+        {
+            try
+            {
+                hibernateDAO.closeSession();
+            }
+            catch (Exception e)
+            {
+                rollbackQueries(stack, category, e, hibernateDAO);
+            }
+        }
+        logDebug("persistCategory", "exiting the method");
+        return category;
+    }
+    
+    public CategoryInterface deleteCategory(CategoryInterface categoryInterface) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
+    {
+        logDebug("deleteCategory", "entering the method");
+        Category category = (Category) categoryInterface;
+        
+        HibernateDAO hibernateDAO = (HibernateDAO) DAOFactory.getInstance().getDAO(
+                Constants.HIBERNATE_DAO);
+        Stack stack = new Stack();
+        
+        try
+        {
+            hibernateDAO.openSession(null);
+            
+            if (category.getId() != null)
+                hibernateDAO.delete(category);
+        
+            hibernateDAO.commit();
+        }
+        catch (Exception e)
+        {
+            rollbackQueries(stack, category, e, hibernateDAO);
+        
+            if (e instanceof DynamicExtensionsApplicationException)
+            {
+                throw (DynamicExtensionsApplicationException) e;
+            }
+            else
+            {
+                throw new DynamicExtensionsSystemException(e.getMessage(), e);
+            }
+        }
+        finally
+        {
+            try
+            {
+                hibernateDAO.closeSession();
+            }
+            catch (Exception e)
+            {
+                rollbackQueries(stack, category, e, hibernateDAO);
+            }
+        }
+        logDebug("persistCategory", "exiting the method");
+        return category;
+    }
 }
--- src/edu/common/dynamicextensions/entitymanager/CategoryManagerInterface.java
+++ src/edu/common/dynamicextensions/entitymanager/CategoryManagerInterface.java
@@ -1,5 +1,9 @@
 package edu.common.dynamicextensions.entitymanager;
 
+import edu.common.dynamicextensions.domaininterface.CategoryInterface;
+import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+
 /**
 *
 * @author rajesh_patil
@@ -7,5 +11,7 @@ package edu.common.dynamicextensions.entitymanager;
 */
 public interface CategoryManagerInterface
 {
-
+    public CategoryInterface persistCategory(CategoryInterface categoryInterface) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
+    
+    public CategoryInterface deleteCategory(CategoryInterface categoryInterface) throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException;
 }

08fdec13ecd21917abc743390864edc1ea3d7c06
kunal_kamble <kunal_kamble>
2007-12-21 04:28:37 +0000

    added code for initializing id_generator table
    SVN-Revision: 4432

--- src/edu/common/dynamicextensions/util/IdGeneratorUtil.java
+++ src/edu/common/dynamicextensions/util/IdGeneratorUtil.java
@@ -1,89 +1,98 @@
 package edu.common.dynamicextensions.util;
 
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import edu.common.dynamicextensions.domain.DomainObjectFactory;
 import edu.common.dynamicextensions.domain.IdGenerator;
 import edu.common.dynamicextensions.domaininterface.IdGeneratorInterface;
 import edu.wustl.common.util.dbManager.DBUtil;
 import edu.wustl.common.util.logger.Logger;
 
 /**
- * This is singleton util class is used to generate unique ids. 
+ * This is singleton util class is used to generate unique ids.
+ * 
  * @author kunal_kamble
  * @version 1.0
  */
-public class IdGeneratorUtil 
-{
+public class IdGeneratorUtil {
 
-	/**
-	 * Static instance of the id generator.
-	 */
-	private static IdGeneratorUtil idGeneratorUtil;
+    /**
+     * Static instance of the id generator.
+     */
+    private static IdGeneratorUtil idGeneratorUtil;
 
-	/**
-	 * Private constructor used for making this class singleton
-	 */
-	private IdGeneratorUtil() 
-    {
-	}
+    /**
+     * Private constructor used for making this class singleton
+     */
+    private IdGeneratorUtil() {
+    }
 
-	/**
-	 * Returns the instance of the IdGeneratorUtil class.
-	 * @return idGeneratorUtil singleton instance of the IdGeneratorUtil.
-	 */
-	public static synchronized IdGeneratorUtil getInstance() 
-    {
-		if (idGeneratorUtil == null) 
-        {
-			idGeneratorUtil = new IdGeneratorUtil();
-		}
-		return idGeneratorUtil;
-	}
+    /**
+     * Returns the instance of the IdGeneratorUtil class.
+     * 
+     * @return idGeneratorUtil singleton instance of the IdGeneratorUtil.
+     */
+    public static synchronized IdGeneratorUtil getInstance() {
+        if (idGeneratorUtil == null) {
+            idGeneratorUtil = new IdGeneratorUtil();
+        }
+        return idGeneratorUtil;
+    }
 
-	/**
-	 * @return next available id
-	 * @throws Exception any exception occured
-	 */
-	public static synchronized Long getNextUniqeId()
-	{
-		Session session = null;
-		Transaction transaction = null;
-		Long nextAvailableId = null;
+    /**
+     * @return next available id
+     * @throws Exception
+     *             any exception occured
+     */
+    public static synchronized Long getNextUniqeId() {
+        Session session = null;
+        Transaction transaction = null;
+        Long nextAvailableId = null;
 
-		try 
-        {
-			session = DBUtil.currentSession().getSessionFactory().openSession();
-			transaction = session.beginTransaction();
+        try {
+            session = DBUtil.currentSession().getSessionFactory().openSession();
+            transaction = session.beginTransaction();
 
-			Query query = session.createQuery("from " + IdGenerator.class.getName());
-			IdGeneratorInterface idGenerator = (IdGeneratorInterface) query.list().get(0);
-		
-			nextAvailableId = idGenerator.getNextAvailableId();
-			idGenerator.setNextAvailableId(nextAvailableId + 1);
-			
-			// Remove idGenerator instance from the session cache.
-			session.evict(idGenerator);
-			session.update(idGenerator);
-			transaction.commit();
-		} 
-        catch (Exception orgExp) 
-        {
-			try 
-            {
-				transaction.rollback();
-			} 
-            catch (RuntimeException rte) 
-            {
-            	Logger.out.debug("Could not rollback transaction", rte);
-			}
-		} 
-        finally 
-        {
-			session.close();
-		}
+            Query query = session.createQuery("from "
+                    + IdGenerator.class.getName());
+            
+            List list =  query.list();
+            if(list.size() == 0){
+            	nextAvailableId  = new Long(1);
+            	IdGeneratorInterface idGeneratorObject = DomainObjectFactory.getInstance().createIdGenerator();
+            	
+            	idGeneratorObject.setNextAvailableId(new Long(2));
+            	idGeneratorObject.setId(new Long(1));
+            	
+            	session.save(idGeneratorObject);
+            	transaction.commit();
+            	
+            	return nextAvailableId;
+            }
+            
+            IdGeneratorInterface idGenerator = (IdGeneratorInterface) query.list().get(0);
+            
+            nextAvailableId = idGenerator.getNextAvailableId();
+            idGenerator.setNextAvailableId(nextAvailableId + 1);
 
-		return nextAvailableId;
-	}
+            // Remove idGenerator instance from the session cache.
+            session.evict(idGenerator);
+            session.update(idGenerator);
+            transaction.commit();
+        } catch (Exception orgExp) {
+            try {
+                transaction.rollback();
+            } catch (RuntimeException rte) {
+                Logger.out.debug("Could not rollback transaction", rte);
+            }
+        } finally {
+            session.close();
+        }
+
+        return nextAvailableId;
+    }
 }

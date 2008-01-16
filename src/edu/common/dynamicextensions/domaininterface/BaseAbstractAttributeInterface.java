684162367ade6b401ada77908313bd9c1853fe40
kunal_kamble <kunal_kamble>
2008-01-16 10:37:01 +0000

    added getRuleCollection method
    SVN-Revision: 5037

--- src/edu/common/dynamicextensions/domaininterface/BaseAbstractAttributeInterface.java
+++ src/edu/common/dynamicextensions/domaininterface/BaseAbstractAttributeInterface.java
@@ -1,10 +1,21 @@
 package edu.common.dynamicextensions.domaininterface;
 
+import java.util.Collection;
+
+import edu.common.dynamicextensions.domaininterface.validationrules.RuleInterface;
+
+
 /**
  * This is an interface extended by CategoryAttribute, AbstractAttribute.
  * This class stores basic information needed for metadata objects.
  */
 public interface BaseAbstractAttributeInterface extends AbstractMetadataInterface
 {
+	
+	/**
+	 * This method returns the Collection of rules.
+	 * @return Collection the ruleCollection associated with the Attribute.
+	 */
+	Collection<RuleInterface> getRuleCollection();
 
 }

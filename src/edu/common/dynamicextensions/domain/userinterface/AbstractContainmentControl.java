7eb7b47eca54894e3dc0e8e87760a4e94e08d6e6
kunal_kamble <kunal_kamble>
2008-01-18 08:49:32 +0000

    Added implementation of generating html in edit and view mode
    SVN-Revision: 5087

--- src/edu/common/dynamicextensions/domain/userinterface/AbstractContainmentControl.java
+++ src/edu/common/dynamicextensions/domain/userinterface/AbstractContainmentControl.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import edu.common.dynamicextensions.domaininterface.BaseAbstractAttributeInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.AbstractContainmentControlInterface;
-import edu.common.dynamicextensions.domaininterface.userinterface.AssociationControlInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.common.dynamicextensions.ui.webui.util.UserInterfaceiUtility;
@@ -23,6 +22,12 @@ public abstract class AbstractContainmentControl extends Control implements Abst
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Parent container 
+	 */
+	protected ContainerInterface container;
+
 
 	public String generateEditModeHTML() throws DynamicExtensionsSystemException
 	{
@@ -55,13 +60,50 @@ public abstract class AbstractContainmentControl extends Control implements Abst
 		return subContainerHTML;
 	}
 
-	protected abstract String generateViewModeHTML() throws DynamicExtensionsSystemException;
+	protected String generateViewModeHTML() throws DynamicExtensionsSystemException
+	{
+		String subContainerHTML = "";
+		if (isCardinalityOneToMany())
+		{
+			List<Map<BaseAbstractAttributeInterface, Object>> valueMapList = (List<Map<BaseAbstractAttributeInterface, Object>>) value;
+			String oldMode = this.getContainer().getMode();
+			this.getContainer().setMode("view");
+			subContainerHTML = this.getContainer().generateControlsHTMLAsGrid(valueMapList);
+			this.getContainer().setMode(oldMode);
+		}
+		else
+		{
+			if (value != null && ((List) value).size() > 0)
+			{
+				Map<BaseAbstractAttributeInterface, Object> displayContainerValueMap = ((List<Map<BaseAbstractAttributeInterface, Object>>) value).get(0);
+				this.getContainer().setContainerValueMap(displayContainerValueMap);
+			}
+			this.getContainer().setShowAssociationControlsAsLink(true);
+			String oldMode = this.getContainer().getMode();
+			this.getContainer().setMode("view");
+			subContainerHTML = this.getContainer().generateControlsHTML();
+			this.getContainer().setMode(oldMode);
+		}
+		return subContainerHTML;
+	}
 
 	/**
-	 * 
+	 * @see edu.common.dynamicextensions.domaininterface.userinterface.ContainmentAssociationControlInterface#generateLinkHTML()
 	 */
-	protected ContainerInterface container;
+	public String generateLinkHTML() throws DynamicExtensionsSystemException
+	{
+		String detailsString = "Details";
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("<a href='#' style='cursor:hand' class='" + cssClass + "' ");
+		stringBuffer.append("onclick='showChildContainerInsertDataPage(");
+		stringBuffer.append(this.getParentContainer().getIncontextContainer().getId() + ",this");
+		stringBuffer.append(")'>");
+		stringBuffer.append(detailsString);
+		stringBuffer.append("</a>");
 
+		return stringBuffer.toString();
+	}
+	
 	/**
 	 * @return container
 	 * @hibernate.many-to-one cascade="save-update" column="CONTAINER_ID" class="edu.common.dynamicextensions.domain.userinterface.Container" constrained="true"

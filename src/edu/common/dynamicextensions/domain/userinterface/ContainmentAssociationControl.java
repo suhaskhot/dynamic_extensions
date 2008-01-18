6868235ce95e0467f6274bc73f890c3a932f8dc7
kunal_kamble <kunal_kamble>
2008-01-18 08:49:47 +0000

    Removed method which are already implemented in AbstractContainmentControl
    SVN-Revision: 5088

--- src/edu/common/dynamicextensions/domain/userinterface/ContainmentAssociationControl.java
+++ src/edu/common/dynamicextensions/domain/userinterface/ContainmentAssociationControl.java
@@ -8,17 +8,10 @@
 
 package edu.common.dynamicextensions.domain.userinterface;
 
-import java.util.List;
-import java.util.Map;
-
 import edu.common.dynamicextensions.domaininterface.AssociationInterface;
-import edu.common.dynamicextensions.domaininterface.BaseAbstractAttributeInterface;
 import edu.common.dynamicextensions.domaininterface.RoleInterface;
-import edu.common.dynamicextensions.domaininterface.userinterface.AssociationControlInterface;
-import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.ContainmentAssociationControlInterface;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
-import edu.common.dynamicextensions.ui.webui.util.UserInterfaceiUtility;
 import edu.common.dynamicextensions.util.global.Constants.Cardinality;
 
 /**
@@ -61,49 +54,7 @@ public class ContainmentAssociationControl extends AbstractContainmentControl im
 		return UserInterfaceiUtility.isCardinalityOneToMany(this);
 	}
 */
-	protected String generateViewModeHTML() throws DynamicExtensionsSystemException
-	{
-		String subContainerHTML = "";
-		if (isCardinalityOneToMany())
-		{
-			List<Map<BaseAbstractAttributeInterface, Object>> valueMapList = (List<Map<BaseAbstractAttributeInterface, Object>>) value;
-			String oldMode = this.getContainer().getMode();
-			this.getContainer().setMode("view");
-			subContainerHTML = this.getContainer().generateControlsHTMLAsGrid(valueMapList);
-			this.getContainer().setMode(oldMode);
-		}
-		else
-		{
-			if (value != null && ((List) value).size() > 0)
-			{
-				Map<BaseAbstractAttributeInterface, Object> displayContainerValueMap = ((List<Map<BaseAbstractAttributeInterface, Object>>) value).get(0);
-				this.getContainer().setContainerValueMap(displayContainerValueMap);
-			}
-			this.getContainer().setShowAssociationControlsAsLink(true);
-			String oldMode = this.getContainer().getMode();
-			this.getContainer().setMode("view");
-			subContainerHTML = this.getContainer().generateControlsHTML();
-			this.getContainer().setMode(oldMode);
-		}
-		return subContainerHTML;
-	}
-
-	/**
-	 * @see edu.common.dynamicextensions.domaininterface.userinterface.ContainmentAssociationControlInterface#generateLinkHTML()
-	 */
-	public String generateLinkHTML() throws DynamicExtensionsSystemException
-	{
-		String detailsString = "Details";
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("<a href='#' style='cursor:hand' class='" + cssClass + "' ");
-		stringBuffer.append("onclick='showChildContainerInsertDataPage(");
-		stringBuffer.append(this.getParentContainer().getIncontextContainer().getId() + ",this");
-		stringBuffer.append(")'>");
-		stringBuffer.append(detailsString);
-		stringBuffer.append("</a>");
-
-		return stringBuffer.toString();
-	}
+	
 
 	/**
 	 * This method returns true if the cardinality of the Containment Association is One to Many.

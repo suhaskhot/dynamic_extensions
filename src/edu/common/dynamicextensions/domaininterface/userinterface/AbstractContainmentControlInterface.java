dacbcb3275cc7bba74afc7f3f6e47ff784f8357a
kunal_kamble <kunal_kamble>
2008-01-21 13:43:31 +0000

    added method declarations 	-generateEditModeHTML() 	-generateViewModeHTML() 	-generateLinkHTML()
    SVN-Revision: 5133

--- src/edu/common/dynamicextensions/domaininterface/userinterface/AbstractContainmentControlInterface.java
+++ src/edu/common/dynamicextensions/domaininterface/userinterface/AbstractContainmentControlInterface.java
@@ -1,4 +1,7 @@
+
 package edu.common.dynamicextensions.domaininterface.userinterface;
+
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 
 public interface AbstractContainmentControlInterface extends ControlInterface
 {
@@ -6,12 +9,32 @@ public interface AbstractContainmentControlInterface extends ControlInterface
 	 * @return container
 	 */
 	ContainerInterface getContainer();
-	
+
 	/**
 	 * @param container The container to set.
 	 */
 	void setContainer(ContainerInterface container);
-	
+
+	/**
+	 * @return
+	 */
 	public boolean isCardinalityOneToMany();
+
+	/**
+	 * @return
+	 * @throws DynamicExtensionsSystemException
+	 */
+	public String generateEditModeHTML() throws DynamicExtensionsSystemException;
+
+	/**
+	 * @return
+	 * @throws DynamicExtensionsSystemException
+	 */
+	public String generateViewModeHTML() throws DynamicExtensionsSystemException;
+
+	/**
+	 * @see edu.common.dynamicextensions.domaininterface.userinterface.ContainmentAssociationControlInterface#generateLinkHTML()
+	 */
+	public String generateLinkHTML() throws DynamicExtensionsSystemException;
 
 }

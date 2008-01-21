a57147a22d2f34fabb406285cf42b1689fabce2a
sujay_narkar <sujay_narkar>
2008-01-21 05:56:08 +0000

    addCategoryAttribute(CategoryAttributeInterface categoryAttributeInterface) method implementation added
    SVN-Revision: 5107

--- src/edu/common/dynamicextensions/domain/CategoryEntity.java
+++ src/edu/common/dynamicextensions/domain/CategoryEntity.java
@@ -213,7 +213,7 @@ public class CategoryEntity extends AbstractEntity implements CategoryEntityInte
 	 */
 	public void addCategoryAttribute(CategoryAttributeInterface categoryAttributeInterface)
 	{
-
+		categoryAttributeCollection.add(categoryAttributeInterface); 
 	}
 
 	/**

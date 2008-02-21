4f052005d6f6f8329a62053decb16135d7cf9c40
mandar_shidhore <mandar_shidhore>
2008-02-21 10:37:34 +0000

    -- test cases for : vitals category and vitals category with permissible values attribute
    SVN-Revision: 5891

--- test/edu/common/dynamicextensions/categoryManager/TestCategoryDemo.java
+++ test/edu/common/dynamicextensions/categoryManager/TestCategoryDemo.java
@@ -4,15 +4,25 @@
 
 package edu.common.dynamicextensions.categoryManager;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 import edu.common.dynamicextensions.categoryManager.CategoryHelperInterface.ControlEnum;
+import edu.common.dynamicextensions.domain.DomainObjectFactory;
 import edu.common.dynamicextensions.domain.EntityGroup;
+import edu.common.dynamicextensions.domain.StringAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.StringValue;
+import edu.common.dynamicextensions.domain.UserDefinedDE;
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.common.dynamicextensions.domaininterface.CategoryInterface;
 import edu.common.dynamicextensions.domaininterface.EntityGroupInterface;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.domaininterface.PermissibleValueInterface;
+import edu.common.dynamicextensions.domaininterface.UserDefinedDEInterface;
 import edu.common.dynamicextensions.domaininterface.userinterface.ContainerInterface;
+import edu.common.dynamicextensions.entitymanager.EntityGroupManager;
 import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.common.dynamicextensions.util.DynamicExtensionsBaseTestCase;
@@ -88,6 +98,64 @@ public class TestCategoryDemo extends DynamicExtensionsBaseTestCase
 
 		return entityGroup;
 	}
+	
+	/**
+	 * Add bmi attribute to vitals entity and add permissible values to this attribute. 
+	 *
+	 */
+	public void testAddPermissibleValuesToVitalsEntityModel()
+	{
+		try
+		{
+			DomainObjectFactory factory = DomainObjectFactory.getInstance();
+
+			EntityGroupInterface entityGroup = retrieveEntityGroup("gcrc");
+
+			// Get the VitalSigns entity from entity group.
+			EntityInterface vitals = entityGroup.getEntityByName("VitalSigns");
+
+			AttributeInterface bmi = factory.createStringAttribute();
+			bmi.setName("BMI");
+			((StringAttributeTypeInformation) bmi.getAttributeTypeInformation()).setSize(40);
+
+			vitals.addAbstractAttribute(bmi);
+
+			// Create permissible values for tumourTissueSite.
+			UserDefinedDEInterface userDefinedDE = factory.createUserDefinedDE();
+
+			PermissibleValueInterface permissibleValue1 = factory.createStringValue();
+			((StringValue) permissibleValue1).setValue("Underweight: 18.5 or below");
+
+			PermissibleValueInterface permissibleValue2 = factory.createStringValue();
+			((StringValue) permissibleValue2).setValue("Healthy Weight: 18.5 - 24.9");
+
+			PermissibleValueInterface permissibleValue3 = factory.createStringValue();
+			((StringValue) permissibleValue3).setValue("Overweight: 25.0 - 29.9");
+
+			PermissibleValueInterface permissibleValue4 = factory.createStringValue();
+			((StringValue) permissibleValue4).setValue("Obese: 30.0 and above");
+
+			userDefinedDE.addPermissibleValue(permissibleValue1);
+			userDefinedDE.addPermissibleValue(permissibleValue2);
+			userDefinedDE.addPermissibleValue(permissibleValue3);
+			userDefinedDE.addPermissibleValue(permissibleValue4);
+
+			StringAttributeTypeInformation bmiTypeInfo = (StringAttributeTypeInformation) bmi.getAttributeTypeInformation();
+			bmiTypeInfo.setDataElement(userDefinedDE);
+
+			EntityGroupManager.getInstance().persistEntityGroup(entityGroup);
+		}
+		catch (DynamicExtensionsSystemException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+		catch (DynamicExtensionsApplicationException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
 
 	/**
 	 * Create a category from vitals entity and its attributes.
@@ -108,7 +176,7 @@ public class TestCategoryDemo extends DynamicExtensionsBaseTestCase
 			CategoryInterface category = categoryHelper.createCategory("Vitals Category");
 
 			// Create category entity from VitalSigns entity.
-			ContainerInterface vitalsContainer = categoryHelper.createCategoryEntityAndContainer(vitals);
+			ContainerInterface vitalsContainer = categoryHelper.createCategoryEntityAndContainer(vitals, "Vitals");
 
 			// Set the root category entity.
 			categoryHelper.setRootCategoryEntity(vitalsContainer, category);
@@ -136,5 +204,64 @@ public class TestCategoryDemo extends DynamicExtensionsBaseTestCase
 		}
 	}
 
+	/**
+	 * Create a category from vitals entity and its attributes.
+	 * Select a subset of permissible values for one of its category attributes.
+	 */
+	public void testCreateVitalsCategoryWithAttributeHavingPermissibleValues()
+	{
+		try
+		{
+			EntityGroupInterface entityGroup = retrieveEntityGroup("gcrc");
 
+			// Get the VitalSigns entity from entity group.
+			EntityInterface vitals = entityGroup.getEntityByName("VitalSigns");
+
+			// Create category.
+			CategoryHelperInterface categoryHelper = new CategoryHelper();
+
+			CategoryInterface category = categoryHelper.createCategory("Vitals Category");
+
+			// Create category entity from VitalSigns entity.
+			ContainerInterface vitalsContainer = categoryHelper.createCategoryEntityAndContainer(vitals, "Vitals");
+
+			// Set the root category entity.
+			categoryHelper.setRootCategoryEntity(vitalsContainer, category);
+
+			// Create User defined DE.
+			UserDefinedDEInterface userDefinedDE = (UserDefinedDE) vitals.getAttributeByName("BMI").getAttributeTypeInformation().getDataElement();
+			List<String> permissibleValues = new ArrayList<String>();
+
+			for (PermissibleValueInterface pv : userDefinedDE.getPermissibleValueCollection())
+			{
+				if (!(pv.getValueAsObject().toString().equals("Obese: 30.0 and above")))
+				{
+					permissibleValues.add(pv.getValueAsObject().toString());
+				}
+			}
+
+			// Create category attribute(s) for VitalSigns category entity.
+			categoryHelper.addControl(vitals, "heartRate", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Pulse");
+			categoryHelper.addControl(vitals, "diastolicBloodPressure", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Diastolic BP");
+			categoryHelper.addControl(vitals, "systolicBloodPressure", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Systolic BP");
+			categoryHelper.addControl(vitals, "weight", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Weight (kg)");
+			categoryHelper.addControl(vitals, "height", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Height (cm)");
+			categoryHelper.addControl(vitals, "respiratoryRate", vitalsContainer, ControlEnum.TEXT_FIELD_CONTROL, "Respiration");
+			categoryHelper.addControl(vitals, "BMI", vitalsContainer, ControlEnum.LIST_BOX_CONTROL, "BMI", permissibleValues);
+
+			// Save the category.
+			categoryHelper.saveCategory(category);
+		}
+		catch (DynamicExtensionsSystemException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+		catch (DynamicExtensionsApplicationException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
 }

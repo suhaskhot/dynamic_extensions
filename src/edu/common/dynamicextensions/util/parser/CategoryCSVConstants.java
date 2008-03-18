74e3e2497714a2f6ba14d6e14ece9ff90e525e02
kunal_kamble <kunal_kamble>
2008-03-18 08:18:50 +0000

    new interface added
    SVN-Revision: 6209

---
+++ src/edu/common/dynamicextensions/util/parser/CategoryCSVConstants.java
@@ -0,0 +1,39 @@
+package edu.common.dynamicextensions.util.parser;
+
+/**
+ * @author kunal_kamble
+ * This interface defines all the keywords used in the 
+ * csv file used for the category creation.
+ *
+ */
+public interface CategoryCSVConstants
+{
+	/**
+	 * This keyword used to mark the begining of 
+	 * the new category defination. 
+	 */
+	String FORM_DEFINITION = "Form_Definition";
+
+	/**
+	 * This keyword is used before specifing the permissible values.
+	 */
+	String PERMISSIBLE_VALUES = "Permissible_Values";
+
+	/**
+	 * This keyword is used before specifing the permissible values 
+	 * file name, where permissible values are line seperated.
+	 */
+	String PERMISSIBLE_VALUES_FILE = "Permissible_Values_File";
+	
+	/**
+	 * This keyword is used before defining the options available for 
+	 * a control. 
+	 */
+	String OPTIONS = "options";
+	
+	/**
+	 * This keyword used to define the lable for the form
+	 */
+	String DISPLAY_LABLE = "Display_Label";
+
+}

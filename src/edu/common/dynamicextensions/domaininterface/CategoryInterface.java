c975214c24e6ac9449b2baf5a6bfdae5742ee718
kunal_kamble <kunal_kamble>
2008-03-28 10:13:50 +0000

    new method added: getCategoryEntityByName
    SVN-Revision: 6316

--- src/edu/common/dynamicextensions/domaininterface/CategoryInterface.java
+++ src/edu/common/dynamicextensions/domaininterface/CategoryInterface.java
@@ -12,5 +12,13 @@ public interface CategoryInterface extends AbstractMetadataInterface {
      * @param rootCategoryElement
      */
     public void setRootCategoryElement(CategoryEntityInterface rootCategoryElement);
+    
+    /**
+     * @param categoryEntity
+     * @param categoryEntityName
+     * @return
+     */
+    public CategoryEntityInterface getCategoryEntityByName(CategoryEntityInterface categoryEntity,
+    		String categoryEntityName);
 
 }

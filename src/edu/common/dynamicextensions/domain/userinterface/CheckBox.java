d652cb97bfd57ce459ab12cc1bf661d462948236
mandar_shidhore <mandar_shidhore>
2008-04-02 09:27:51 +0000

    -- check box in edit mode was not retaining data entry record. Changes made to generateEditModeHTML(....) method
    SVN-Revision: 6358

--- src/edu/common/dynamicextensions/domain/userinterface/CheckBox.java
+++ src/edu/common/dynamicextensions/domain/userinterface/CheckBox.java
@@ -16,77 +16,70 @@ import edu.common.dynamicextensions.util.DynamicExtensionsUtility;
 public class CheckBox extends Control implements CheckBoxInterface
 {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Empty Constructor
-     */
-    public CheckBox()
-    {
-    }
+	/**
+	 * Empty Constructor
+	 */
+	public CheckBox()
+	{
+	}
 
-    /* (non-Javadoc)
-     * @see edu.common.dynamicextensions.domain.userinterface.Control#generateEditModeHTML()
-     */
-    protected String generateEditModeHTML() throws DynamicExtensionsSystemException
-    {
-        String checked = (String) this.value;
-        String htmlString = "";
-        if (this.value == null)
-        {
-            //checked = ControlsUtility.getDefaultValue(this.getAbstractAttribute());
-        	checked = this.getAttibuteMetadataInterface().getDefaultValue();
-        }
+	/* (non-Javadoc)
+	 * @see edu.common.dynamicextensions.domain.userinterface.Control#generateEditModeHTML()
+	 */
+	protected String generateEditModeHTML() throws DynamicExtensionsSystemException
+	{
+		String checked = (String) this.value;
+		String htmlString = "";
+		if (this.value == null)
+		{
+			checked = this.getAttibuteMetadataInterface().getDefaultValue();
+		}
 
-        String htmlComponentName = getHTMLComponentName();
-        if (checked != null && checked.equals("true"))
-        {
-            htmlString = "<input type='checkbox' class='" + this.cssClass + "' name='"
-                    + htmlComponentName + "' checkedValue='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(true) + "' uncheckedValue='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(false) + "'" + "value='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(true) + "' " + "id='"
-                    + htmlComponentName + "'"
-                    + "checked"
-                    + " onclick='changeValueForCheckBox(this);'>";
-        }
-        else
-        {
-            htmlString = "<input type='checkbox' class='" + this.cssClass + "' name='"
-                    + htmlComponentName + "' checkedValue='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(true) + "' uncheckedValue='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(false) + "'" + "value='"
-                    + DynamicExtensionsUtility.getValueForCheckBox(false) + "' " + "id='"
-                    + htmlComponentName + "' onclick='changeValueForCheckBox(this);'>";
-        }
+		String htmlComponentName = getHTMLComponentName();
+		if (checked != null && checked.equals("true") || checked.equals("1"))
+		{
+			htmlString = "<input type='checkbox' class='" + this.cssClass + "' name='" + htmlComponentName + "' checkedValue='"
+					+ DynamicExtensionsUtility.getValueForCheckBox(true) + "' uncheckedValue='" + DynamicExtensionsUtility.getValueForCheckBox(false)
+					+ "'" + "value='" + DynamicExtensionsUtility.getValueForCheckBox(true) + "' " + "id='" + htmlComponentName + "'" + "checked"
+					+ " onclick='changeValueForCheckBox(this);'>";
+		}
+		else
+		{
+			htmlString = "<input type='checkbox' class='" + this.cssClass + "' name='" + htmlComponentName + "' checkedValue='"
+					+ DynamicExtensionsUtility.getValueForCheckBox(true) + "' uncheckedValue='" + DynamicExtensionsUtility.getValueForCheckBox(false)
+					+ "'" + "value='" + DynamicExtensionsUtility.getValueForCheckBox(false) + "' " + "id='" + htmlComponentName
+					+ "' onclick='changeValueForCheckBox(this);'>";
+		}
 
-        return htmlString;
-    }
+		return htmlString;
+	}
 
-    /* (non-Javadoc)
-     * @see edu.common.dynamicextensions.domain.userinterface.Control#generateViewModeHTML()
-     */
-    protected String generateViewModeHTML() throws DynamicExtensionsSystemException
-    {
-        String htmlString = "&nbsp;";
-        if (value != null)
-        {
-            String checked = (String) this.value;
-            htmlString = "<input type='checkbox' class='" + cssClass + "' "
-                    + DynamicExtensionsUtility.getCheckboxSelectionValue(checked) + " disabled>";
-        }
-        return htmlString;
-    }
+	/* (non-Javadoc)
+	 * @see edu.common.dynamicextensions.domain.userinterface.Control#generateViewModeHTML()
+	 */
+	protected String generateViewModeHTML() throws DynamicExtensionsSystemException
+	{
+		String htmlString = "&nbsp;";
+		if (value != null)
+		{
+			String checked = (String) this.value;
+			htmlString = "<input type='checkbox' class='" + cssClass + "' " + DynamicExtensionsUtility.getCheckboxSelectionValue(checked)
+					+ " disabled>";
+		}
+		return htmlString;
+	}
 
-    /**
-     * This method sets the corresponding AbstractAttribute of this Control.
-     * @param abstractAttribute AbstractAttribute to be set.
-     */
-    public void setAttribute(AbstractAttributeInterface abstractAttribute)
-    {
-    }
+	/**
+	 * This method sets the corresponding AbstractAttribute of this Control.
+	 * @param abstractAttribute AbstractAttribute to be set.
+	 */
+	public void setAttribute(AbstractAttributeInterface abstractAttribute)
+	{
+	}
 
 }

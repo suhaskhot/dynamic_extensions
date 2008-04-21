60c7243f2da4ae42f0606522b95bfb61de69c757
kunal_kamble <kunal_kamble>
2008-04-21 13:07:50 +0000

    new protected variable added CategoryValidator categoryValidator now methods getEntityName and getMultiplicity will parse ass well as validate entity name and multiplicity respectively
    SVN-Revision: 6550

--- src/edu/common/dynamicextensions/util/parser/CategoryFileParser.java
+++ src/edu/common/dynamicextensions/util/parser/CategoryFileParser.java
@@ -1,3 +1,4 @@
+
 package edu.common.dynamicextensions.util.parser;
 
 import java.io.IOException;
@@ -6,6 +7,7 @@ import java.util.Map;
 
 import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.common.dynamicextensions.util.FileReader;
+import edu.common.dynamicextensions.validation.category.CategoryValidator;
 
 /**
  * @author kunal_kamble
@@ -13,6 +15,8 @@ import edu.common.dynamicextensions.util.FileReader;
  */
 public abstract class CategoryFileParser extends FileReader
 {
+	protected CategoryValidator categoryValidator;
+
 	/**
 	 * @param filePath
 	 * @throws DynamicExtensionsSystemException
@@ -26,45 +30,46 @@ public abstract class CategoryFileParser extends FileReader
 	 * @return current line number in the category file
 	 */
 	public abstract long getLineNumber();
-	
+
 	/**
 	 * @return false if end of category file 
 	 * otherwise reads the next line.
 	 * @throws IOException
 	 */
-	public abstract boolean readNext() throws IOException;	
+	public abstract boolean readNext() throws IOException;
 
 	/**
 	 * @return category name
 	 */
 	public abstract String getCategoryName();
-	
+
 	/**
 	 * @return entity group name
 	 */
 	public abstract String getEntityGroupName();
-	
+
 	/**
 	 * @return map with key as entity name and 
 	 * value as its complete path from the root 
 	 * @throws DynamicExtensionsSystemException
 	 */
 	public abstract Map<String, List<String>> getPaths() throws DynamicExtensionsSystemException;
-	
+
 	/**
 	 * @return form label
 	 */
 	public abstract String getDisplyLable();
-	
+
 	/**
 	 * @return 
 	 */
 	public abstract boolean isShowCaption();
-	
+
 	/**
-	 * @return entity name
+	 * @return
+	 * @throws DynamicExtensionsSystemException
 	 */
-	public abstract String getEntityName();
+	public abstract String getEntityName() throws DynamicExtensionsSystemException;
 
 	/**
 	 * @return attribute name
@@ -75,12 +80,12 @@ public abstract class CategoryFileParser extends FileReader
 	 * @return type of control
 	 */
 	public abstract String getControlType();
-	
+
 	/** 
 	 * @return control label
 	 */
-	public abstract  String getControlCaption();
-	
+	public abstract String getControlCaption();
+
 	/**
 	 * @return map with key as control property and
 	 * value as value for the property
@@ -92,36 +97,55 @@ public abstract class CategoryFileParser extends FileReader
 	 * @throws DynamicExtensionsSystemException
 	 */
 	public abstract List<String> getPermissibleValues() throws DynamicExtensionsSystemException;
-	
+
 	/**
 	 * @return 
 	 */
 	public abstract boolean hasDisplayLable();
-	
+
 	/**
 	 * @return
 	 */
 	public abstract boolean hasFormDefination();
-	
+
 	/**
 	 * @return
 	 */
 	public abstract boolean hasSubcategory();
-	
+
 	/**
 	 * @return target container caption
 	 */
 	public abstract String getTargetContainerCaption();
-	
+
 	/**
 	 * @return multiplicity
 	 */
-	public abstract String getMultiplicity();
-	
+	/**
+	 * @return
+	 * @throws DynamicExtensionsSystemException
+	 */
+	public abstract String getMultiplicity() throws DynamicExtensionsSystemException;
+
 	/**
 	 * @return category path
 	 */
-	public abstract String[] getCategoryPaths();	
-	
-	
+	public abstract String[] getCategoryPaths();
+
+	/**
+	 * @return
+	 */
+	public CategoryValidator getCategoryValidator()
+	{
+		return categoryValidator;
+	}
+
+	/**
+	 * @param categoryValidator
+	 */
+	public void setCategoryValidator(CategoryValidator categoryValidator)
+	{
+		this.categoryValidator = categoryValidator;
+	}
+
 }

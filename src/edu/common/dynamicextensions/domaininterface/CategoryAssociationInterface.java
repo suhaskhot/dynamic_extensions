ce4cbf71e096679b60af76de485c80a1932e07e7
mandar_shidhore <mandar_shidhore>
2008-05-05 05:29:28 +0000

    -- following method signatures added:
    public ConstraintPropertiesInterface getConstraintProperties();
    public void setConstraintProperties(ConstraintPropertiesInterface constraintProperties);
    
    
    SVN-Revision: 6614

--- src/edu/common/dynamicextensions/domaininterface/CategoryAssociationInterface.java
+++ src/edu/common/dynamicextensions/domaininterface/CategoryAssociationInterface.java
@@ -1,36 +1,48 @@
 
 package edu.common.dynamicextensions.domaininterface;
 
+import edu.common.dynamicextensions.domaininterface.databaseproperties.ConstraintPropertiesInterface;
+
 
 /**
- * 
+ *
  * @author sujay_narkar
  *
  */
 public interface CategoryAssociationInterface extends BaseAbstractAttributeInterface
 {
 	/**
-	 * 
+	 *
 	 * @return
 	 */
 	public CategoryEntityInterface getCategoryEntity();
 
 	/**
-	 * 
+	 *
 	 * @param categoryEntity
 	 */
 	public void setCategoryEntity(CategoryEntityInterface categoryEntity);
-	
+
 	/**
-	 * 
+	 *
 	 * @return
 	 */
 	public CategoryEntityInterface getTargetCategoryEntity();
-	
+
 	/**
-	 * 
+	 *
 	 * @param targetCategoryEntity
 	 */
 	public void setTargetCategoryEntity(CategoryEntityInterface targetCategoryEntity);
+	/**
+	 * This method returns the ConstraintProperties of the Association.
+	 * @return the ConstraintProperties of the Association.
+	 */
+	public ConstraintPropertiesInterface getConstraintProperties();
+	/**
+	 * This method sets the constraintProperties to the given ContraintProperties.
+	 * @param constraintProperties the constraintProperties to be set.
+	 */
+	public void setConstraintProperties(ConstraintPropertiesInterface constraintProperties);
 
 }

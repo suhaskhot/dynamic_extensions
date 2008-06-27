2ace4be589e6202925558c2c7eb72d3cefa01972
mandar_shidhore <mandar_shidhore>
2008-06-27 09:09:33 +0000

    -- data precision and scale validation against all data types supported added
    SVN-Revision: 7510

--- src/edu/common/dynamicextensions/validation/NumberValidator.java
+++ src/edu/common/dynamicextensions/validation/NumberValidator.java
@@ -1,19 +1,23 @@
 
 package edu.common.dynamicextensions.validation;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import edu.common.dynamicextensions.domain.DateAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.DoubleAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.FloatAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.IntegerAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.LongAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.NumericAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.ShortAttributeTypeInformation;
 import edu.common.dynamicextensions.domaininterface.AttributeMetadataInterface;
 import edu.common.dynamicextensions.domaininterface.AttributeTypeInformationInterface;
+import edu.common.dynamicextensions.entitymanager.DataTypeFactory;
+import edu.common.dynamicextensions.entitymanager.DataTypeInformation;
+import edu.common.dynamicextensions.entitymanager.EntityManagerConstantsInterface;
+import edu.common.dynamicextensions.exception.DataTypeFactoryInitializationException;
 import edu.common.dynamicextensions.exception.DynamicExtensionsValidationException;
 import edu.common.dynamicextensions.util.DynamicExtensionsUtility;
 
@@ -32,33 +36,56 @@ public class NumberValidator implements ValidatorRuleInterface
 	 * @param valueObject the value entered by the user.
 	 * @param parameterMap the parameters of the Rule.
 	 * @throws DynamicExtensionsValidationException if the value is not following the Numeric Rule. 
+	 * @throws DataTypeFactoryInitializationException 
 	 */
-	public boolean validate(AttributeMetadataInterface attribute, Object valueObject,
-			Map<String, String> parameterMap,String controlCaption) throws DynamicExtensionsValidationException
+	public boolean validate(AttributeMetadataInterface attribute, Object valueObject, Map<String, String> parameterMap, String controlCaption) throws DynamicExtensionsValidationException, DataTypeFactoryInitializationException
 	{
 		boolean isValid = true;
-		AttributeTypeInformationInterface attributeTypeInformation = attribute
-				.getAttributeTypeInformation();
-		String attributeName = attribute.getName();
+		AttributeTypeInformationInterface attributeTypeInformation = attribute.getAttributeTypeInformation();
 
 		String value = (String) valueObject;
 		value = value.trim();
+		
+		DataTypeFactory dataTypeFactory = null;
+		
+		try
+		{
+			dataTypeFactory = DataTypeFactory.getInstance();
+		}
+		catch (DataTypeFactoryInitializationException e)
+		{
+			throw new DataTypeFactoryInitializationException(e.getMessage());
+		}
 		
 		if (attributeTypeInformation != null)
 		{
 			try
 			{
-				if (!(DynamicExtensionsUtility.isNumeric((String) valueObject)) )
-						{
-					isValid=false;
-						}
+				if (!(DynamicExtensionsUtility.isNumeric((String) valueObject)))
+				{
+					isValid = false;
+				}
 				else if (attributeTypeInformation instanceof LongAttributeTypeInformation)
 				{
-					checkIntegerNumberValidity(controlCaption, value.trim());
+					checkLongNumberValidity(dataTypeFactory, controlCaption, value);
+				}
+				else if (attributeTypeInformation instanceof IntegerAttributeTypeInformation)
+				{
+					checkIntegerNumberValidity(dataTypeFactory, controlCaption, value);
+				}
+				else if (attributeTypeInformation instanceof ShortAttributeTypeInformation)
+				{
+					checkShortNumberValidity(dataTypeFactory, controlCaption, value);
 				}
 				else if (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
 				{
-					checkRealNumberValidity(controlCaption, value.trim());
+					validateDigitsAfterDecimalAgainstPrecision(attributeTypeInformation, controlCaption, value.trim());
+					checkDoubleNumberValidity(dataTypeFactory, controlCaption, value);
+				}
+				else if (attributeTypeInformation instanceof FloatAttributeTypeInformation)
+				{
+					validateDigitsAfterDecimalAgainstPrecision(attributeTypeInformation, controlCaption, value.trim());
+					checkFloatNumberValidity(dataTypeFactory, controlCaption, value);
 				}
 			}
 			catch (NumberFormatException numberFormatException)
@@ -66,82 +93,234 @@ public class NumberValidator implements ValidatorRuleInterface
 				isValid = false;
 			}
 		}
-		//if(value.isEmpty())
-		if(value.length() == 0)
+
+		if (value.length() == 0)
 		{
 			isValid = true;
 		}
-		if(!isValid)
+		if (!isValid)
 		{
-			throw new DynamicExtensionsValidationException("Validation failed", null,
-					"dynExtn.validation.Number", controlCaption);
+			throw new DynamicExtensionsValidationException("Validation failed", null, "dynExtn.validation.Number", controlCaption);
 		}
 
 		return isValid;
 	}
-
+	
 	/**
-	 * This method checks the validty of the Integer values.
+	 * This method checks the validity of the Long values.
 	 * @param attributeName Name of the Attribute.
 	 * @param value The value to be verified
 	 * @throws DynamicExtensionsValidationException if the value is not in the numeric range of Long.
 	 * @throws NumberFormatException if the value is not of numeric nature.
 	 */
-	private void checkIntegerNumberValidity(String controlCaption, String value)
-			throws DynamicExtensionsValidationException, NumberFormatException
+	private void checkLongNumberValidity(DataTypeFactory dataTypeFactory, String controlCaption, String value) throws DynamicExtensionsValidationException, NumberFormatException
 	{
         if (value.contains("."))
         {
-            if (DynamicExtensionsUtility.isNumeric(value) && (value.indexOf(".") != -1))
-            {
-                //Integer decimalPlaces = 0;
-                StringBuffer decimalFormat = new StringBuffer("#");
-                NumberFormat numberFormat = new DecimalFormat(decimalFormat.toString());
-                value = numberFormat.format(Double.parseDouble(value));
-            }
+        	reportInvalidInput(controlCaption, EntityManagerConstantsInterface.REAL_ATTRIBUTE_TYPE, EntityManagerConstantsInterface.LONG_ATTRIBUTE_TYPE, "dynExtn.validation.Number.errorInput");
         }
+        
+		validatePrecisionAndScale(dataTypeFactory, value, controlCaption, EntityManagerConstantsInterface.LONG_ATTRIBUTE_TYPE);
         
 		BigInteger numberValue = new BigInteger(value);
 		String strLongMin = (new Long(Long.MIN_VALUE)).toString();
 		String strLongMax = (new Long(Long.MAX_VALUE)).toString();
 		BigInteger longMin = new BigInteger(strLongMin);
 		BigInteger longMax = new BigInteger(strLongMax);
-
+		
 		if (numberValue.compareTo(longMin) < 0 || numberValue.compareTo(longMax) > 0)
 		{
-			List<String> placeHolders = new ArrayList<String>();
-			placeHolders.add(controlCaption);
-			placeHolders.add(strLongMin);
-			placeHolders.add(strLongMax);
-			throw new DynamicExtensionsValidationException("Validation failed", null,
-					"dynExtn.validation.Number.Range", placeHolders);
+			reportInvalidInput(controlCaption, strLongMin, strLongMax, "dynExtn.validation.Number.Range");
 		}
 	}
 
 	/**
-	 * This method checks the validty of the Real values.
-	 * @param attributeName Name of the Attribute.
-	 * @param value The value to be verified
-	 * @throws DynamicExtensionsValidationException if the value is not in the numeric range of Double.
-	 * @throws NumberFormatException if the value is not of numeric nature. 
+	 * @param dataTypeFactory
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
 	 */
-	private void checkRealNumberValidity(String controlCaption, String value)
-			throws DynamicExtensionsValidationException, NumberFormatException
+	private void checkIntegerNumberValidity(DataTypeFactory dataTypeFactory, String controlCaption, String value) throws DynamicExtensionsValidationException
 	{
-		BigDecimal numberValue = new BigDecimal(value);
+		if (value.contains("."))
+        {
+        	reportInvalidInput(controlCaption, EntityManagerConstantsInterface.REAL_ATTRIBUTE_TYPE, EntityManagerConstantsInterface.LONG_ATTRIBUTE_TYPE, "dynExtn.validation.Number.errorInput");
+        }
+        
+		validatePrecisionAndScale(dataTypeFactory, value, controlCaption, EntityManagerConstantsInterface.INTEGER_ATTRIBUTE_TYPE);
+        
+		BigInteger numberValue = new BigInteger(value);
+		String strIntegerMin = (new Integer(Integer.MIN_VALUE)).toString();
+		String strIntegerMax = (new Integer(Integer.MAX_VALUE)).toString();
+		BigInteger integerMin = new BigInteger(strIntegerMin);
+		BigInteger integerMax = new BigInteger(strIntegerMax);
+		
+		if (numberValue.compareTo(integerMin) < 0 || numberValue.compareTo(integerMax) > 0)
+		{
+			reportInvalidInput(controlCaption, strIntegerMin, strIntegerMax, "dynExtn.validation.Number.Range");
+		}
+	}
+	
+	/**
+	 * @param dataTypeFactory
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void checkShortNumberValidity(DataTypeFactory dataTypeFactory, String controlCaption, String value) throws DynamicExtensionsValidationException
+	{
+		if (value.contains("."))
+        {
+        	reportInvalidInput(controlCaption, EntityManagerConstantsInterface.REAL_ATTRIBUTE_TYPE, EntityManagerConstantsInterface.LONG_ATTRIBUTE_TYPE, "dynExtn.validation.Number.errorInput");
+        }
+        
+		validatePrecisionAndScale(dataTypeFactory, value, controlCaption, EntityManagerConstantsInterface.SHORT_ATTRIBUTE_TYPE);
+        
+		BigInteger numberValue = new BigInteger(value);
+		String strShortMin = (new Short(Short.MIN_VALUE)).toString();
+		String strShortMax = (new Short(Short.MAX_VALUE)).toString();
+		BigInteger shortMin = new BigInteger(strShortMin);
+		BigInteger shortMax = new BigInteger(strShortMax);
+		
+		if (numberValue.compareTo(shortMin) < 0 || numberValue.compareTo(shortMax) > 0)
+		{
+			reportInvalidInput(controlCaption, strShortMin, strShortMax, "dynExtn.validation.Number.Range");
+		}
+	}
+
+	/**
+	 * @param dataTypeFactory
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 * @throws NumberFormatException
+	 */
+	private void checkDoubleNumberValidity(DataTypeFactory dataTypeFactory, String controlCaption, String value) throws DynamicExtensionsValidationException, NumberFormatException
+	{
+		validatePrecisionAndScale(dataTypeFactory, value, controlCaption, EntityManagerConstantsInterface.DOUBLE_ATTRIBUTE_TYPE);
+		
+		/*Double numberValue = new Double(value);
 		String strDoubleMin = (new Double(-Double.MAX_VALUE)).toString();
 		String strDoubleMax = (new Double(Double.MAX_VALUE)).toString();
-		BigDecimal doubleMin = new BigDecimal(strDoubleMin);
-		BigDecimal doubleMax = new BigDecimal(strDoubleMax);
+		Double doubleMin = new Double(strDoubleMin);
+		Double doubleMax = new Double(strDoubleMax);
 
 		if (numberValue.compareTo(doubleMin) < 0 || numberValue.compareTo(doubleMax) > 0)
 		{
-			List<String> placeHolders = new ArrayList<String>();
-			placeHolders.add(controlCaption);
-			placeHolders.add(strDoubleMin);
-			placeHolders.add(strDoubleMax);
-			throw new DynamicExtensionsValidationException("Validation failed", null,
-					"dynExtn.validation.Number.Range", placeHolders);
+			reportInvalidInput(controlCaption, strDoubleMin, strDoubleMax, "dynExtn.validation.Number.Range");
+		}*/
+	}
+	
+	/**
+	 * @param dataTypeFactory
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void checkFloatNumberValidity(DataTypeFactory dataTypeFactory, String controlCaption, String value) throws DynamicExtensionsValidationException
+	{
+		validatePrecisionAndScale(dataTypeFactory, value, controlCaption, EntityManagerConstantsInterface.FLOAT_ATTRIBUTE_TYPE);
+		
+		/*Float numberValue = new Float(value);
+		String strFloatMin = (new Float(-Float.MAX_VALUE)).toString();
+		String strFloatMax = (new Float(Float.MAX_VALUE)).toString();
+		Float floatMin = new Float(strFloatMin);
+		Float floatMax = new Float(strFloatMax);
+
+		if (numberValue.compareTo(floatMin) < 0 || numberValue.compareTo(floatMax) > 0)
+		{
+			reportInvalidInput(controlCaption, strFloatMin, strFloatMax, "dynExtn.validation.Number.Range");
+		}*/
+	}
+
+	/**
+	 * @param dataTypeFactory
+	 * @param value
+	 * @param controlCaption
+	 * @param dataType
+	 * @throws DynamicExtensionsValidationException
+	 */
+	public void validatePrecisionAndScale(DataTypeFactory dataTypeFactory, String value, String controlCaption, String dataType) throws DynamicExtensionsValidationException
+	{
+		DataTypeInformation dataTypeInfoObject = (DataTypeInformation) dataTypeFactory.getDataTypePrecisionScaleInformation(dataType);
+		
+		if (value != null && dataTypeInfoObject != null)
+		{
+			if (value.contains("."))
+			{
+				int decimalPointIndex = value.indexOf(".");
+				String stringBeforeDecimalPoint = value.substring(0, decimalPointIndex);
+				String stringAfterDecimalPoint = value.substring(decimalPointIndex + 1, value.length());
+				
+				if (stringBeforeDecimalPoint.length() > Integer.parseInt(dataTypeInfoObject.getDigitsBeforeDecimal()) || stringAfterDecimalPoint.length() > Integer.parseInt(dataTypeInfoObject.getDigitsAfterDecimal()))
+				{
+					reportInvalidInput(controlCaption, dataType, dataTypeInfoObject, "dynExtn.validation.Number.PrecisionScale");
+				}
+			}
+			else
+			{
+				if (value.length() > Integer.parseInt(dataTypeInfoObject.getDigitsBeforeDecimal()))
+				{
+					reportInvalidInput(controlCaption, dataType, dataTypeInfoObject, "dynExtn.validation.Number.PrecisionScale");
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param controlCaption
+	 * @param dataType
+	 * @param precisionScaleInfoObject
+	 * @param errorKey
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void reportInvalidInput(String controlCaption, String dataType, DataTypeInformation precisionScaleInfoObject, String errorKey) throws DynamicExtensionsValidationException
+	{
+		List<String> placeHolders = new ArrayList<String>();
+		placeHolders.add(controlCaption +" (type: "+dataType+") ");
+		placeHolders.add(precisionScaleInfoObject.getDigitsBeforeDecimal());
+		placeHolders.add(precisionScaleInfoObject.getDigitsAfterDecimal());
+		throw new DynamicExtensionsValidationException("Validation failed", null, errorKey, placeHolders);
+	}
+	
+	/**
+	 * @param controlCaption
+	 * @param value1
+	 * @param value2
+	 * @param errorKey
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void reportInvalidInput(String controlCaption, String value1, String value2, String errorKey) throws DynamicExtensionsValidationException
+	{
+		List<String> placeHolders = new ArrayList<String>();
+		placeHolders.add(controlCaption);
+		placeHolders.add(value1);
+		placeHolders.add(value2);
+		throw new DynamicExtensionsValidationException("Validation failed", null, errorKey, placeHolders);
+	}
+	
+	/**
+	 * @param attributeTypeInformation
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void validateDigitsAfterDecimalAgainstPrecision(AttributeTypeInformationInterface attributeTypeInformation, String controlCaption, String value) throws DynamicExtensionsValidationException
+	{
+		int decimalPlaces = ((NumericAttributeTypeInformation) attributeTypeInformation).getDecimalPlaces();
+		
+		if (value != null)
+		{
+			if (value.contains("."))
+			{
+				int decimalPointIndex = value.indexOf(".");
+				String stringAfterDecimalPoint = value.substring(decimalPointIndex + 1, value.length());
+				
+				if (stringAfterDecimalPoint.length() > decimalPlaces)
+				{
+					reportInvalidInput(controlCaption, "Number of digits after decimal point", "precision ("+decimalPlaces+")", "dynExtn.validation.Number.numberOfDigitsExceedsPrecision");
+				}
+			}
 		}
 	}
 
--- src/edu/common/dynamicextensions/validation/RangeValidator.java
+++ src/edu/common/dynamicextensions/validation/RangeValidator.java
@@ -5,12 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 import edu.common.dynamicextensions.domain.DoubleAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.FloatAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.IntegerAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.LongAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.ShortAttributeTypeInformation;
 import edu.common.dynamicextensions.domaininterface.AttributeMetadataInterface;
 import edu.common.dynamicextensions.domaininterface.AttributeTypeInformationInterface;
+import edu.common.dynamicextensions.exception.DataTypeFactoryInitializationException;
 import edu.common.dynamicextensions.exception.DynamicExtensionsValidationException;
+import edu.common.dynamicextensions.ui.util.Constants;
 
 /**
  * This Class validates the range of the numeric data entered by the user. It checks whether the value entered is between the 
@@ -29,26 +35,26 @@ public class RangeValidator implements ValidatorRuleInterface
 	 * @param valueObject the value entered by the user.
 	 * @param parameterMap the parameters of the Rule.
 	 * @throws DynamicExtensionsValidationException if the value is not following the range Rule. 
+	 * @throws DataTypeFactoryInitializationException 
 	 */
-	public boolean validate(AttributeMetadataInterface attribute, Object valueObject,
-			Map<String, String> parameterMap,String controlCaption) throws DynamicExtensionsValidationException
+	public boolean validate(AttributeMetadataInterface attribute, Object valueObject, Map<String, String> parameterMap, String controlCaption)
+			throws DynamicExtensionsValidationException, DataTypeFactoryInitializationException
 	{
 		boolean valid = true;
 
 		/* Check for the validity of the number */
 		NumberValidator numberValidator = new NumberValidator();
-		numberValidator.validate(attribute, valueObject, parameterMap,controlCaption);
+		numberValidator.validate(attribute, valueObject, parameterMap, controlCaption);
 
-		/* Check for the validity of the range of the number against the pre-defined range*/
+		/* Check for the validity of the range of the number against the predefined range*/
 		if (valueObject != null)
 		{
 			if (!((String) valueObject).trim().equals(""))
 			{
-				AttributeTypeInformationInterface attributeTypeInformation = attribute
-						.getAttributeTypeInformation();
+				AttributeTypeInformationInterface attributeTypeInformation = attribute.getAttributeTypeInformation();
+
 				if (attributeTypeInformation != null)
 				{
-					String attributeName = attribute.getName();
 					String value = (String) valueObject;
 
 					Set<Map.Entry<String, String>> parameterSet = parameterMap.entrySet();
@@ -58,14 +64,27 @@ public class RangeValidator implements ValidatorRuleInterface
 						{
 							checkLongValidation(parameter, controlCaption, value);
 						}
+						else if (attributeTypeInformation instanceof IntegerAttributeTypeInformation)
+						{
+							checkIntegerValidation(parameter, controlCaption, value);
+						}
+						else if (attributeTypeInformation instanceof ShortAttributeTypeInformation)
+						{
+							checkShortValidation(parameter, controlCaption, value);
+						}
 						else if (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
 						{
 							checkDoubleValidation(parameter, controlCaption, value);
+						}
+						else if (attributeTypeInformation instanceof FloatAttributeTypeInformation)
+						{
+							checkFloatValidation(parameter, controlCaption, value);
 						}
 					}
 				}
 			}
 		}
+
 		return valid;
 	}
 
@@ -76,77 +95,152 @@ public class RangeValidator implements ValidatorRuleInterface
 	 * @param value the value to be verified.
 	 * @throws DynamicExtensionsValidationException
 	 */
-	private void checkLongValidation(Map.Entry<String, String> parameter, String controlCaption,
-			String value) throws DynamicExtensionsValidationException
+	private void checkLongValidation(Map.Entry<String, String> parameter, String controlCaption, String value)
+			throws DynamicExtensionsValidationException
 	{
 		String parameterName = parameter.getKey();
 		String parameterValue = parameter.getValue();
 
-		List<String> placeHolders = null;
-		if (parameterName.equals("min"))
+		if (parameterName.equals(Constants.MIN_VALUE))
 		{
 			if (Long.parseLong(value) < Long.parseLong(parameterValue))
 			{
-				placeHolders = new ArrayList<String>();
-				placeHolders.add(controlCaption);
-				placeHolders.add(parameterValue);
-
-				throw new DynamicExtensionsValidationException("Validation failed", null,
-						"dynExtn.validation.Range.Minimum", placeHolders);
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Minimum");
 			}
 		}
-		else if (parameterName.equals("max"))
+		else if (parameterName.equals(Constants.MAX_VALUE))
 		{
 			if (Long.parseLong(value) > Long.parseLong(parameterValue))
 			{
-				placeHolders = new ArrayList<String>();
-				placeHolders.add(controlCaption);
-				placeHolders.add(parameterValue);
-
-				throw new DynamicExtensionsValidationException("Validation failed", null,
-						"dynExtn.validation.Range.Maximum", placeHolders);
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Maximum");
 			}
 		}
 	}
 
 	/**
-	 * This method verifies if the number is a proper Double value or not.
-	 * @param parameter the parameter of the rule in form of <ParameterName, Value> pair. 
-	 * @param attributeName the name of the Attribute.
-	 * @param value the value to be verified.
+	 * @param parameter
+	 * @param controlCaption
+	 * @param value
 	 * @throws DynamicExtensionsValidationException
 	 */
-	private void checkDoubleValidation(Map.Entry<String, String> parameter, String controlCaption,
-			String value) throws DynamicExtensionsValidationException
+	private void checkIntegerValidation(Entry<String, String> parameter, String controlCaption, String value)
+			throws DynamicExtensionsValidationException
 	{
 		String parameterName = parameter.getKey();
 		String parameterValue = parameter.getValue();
 
-		List<String> placeHolders = null;
-		if (parameterName.equals("min"))
+		if (parameterName.equals(Constants.MIN_VALUE))
+		{
+			if (Integer.parseInt(value) < Integer.parseInt(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Minimum");
+			}
+		}
+		else if (parameterName.equals(Constants.MAX_VALUE))
+		{
+			if (Integer.parseInt(value) > Integer.parseInt(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Maximum");
+			}
+		}
+	}
+
+	/**
+	 * @param parameter
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void checkShortValidation(Entry<String, String> parameter, String controlCaption, String value)
+			throws DynamicExtensionsValidationException
+	{
+		String parameterName = parameter.getKey();
+		String parameterValue = parameter.getValue();
+
+		if (parameterName.equals(Constants.MIN_VALUE))
+		{
+			if (Short.parseShort(value) < Short.parseShort(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Minimum");
+			}
+		}
+		else if (parameterName.equals(Constants.MAX_VALUE))
+		{
+			if (Short.parseShort(value) > Short.parseShort(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Maximum");
+			}
+		}
+	}
+
+	/**
+	 * @param parameter
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void checkDoubleValidation(Map.Entry<String, String> parameter, String controlCaption, String value)
+			throws DynamicExtensionsValidationException
+	{
+		String parameterName = parameter.getKey();
+		String parameterValue = parameter.getValue();
+
+		if (parameterName.equals(Constants.MIN_VALUE))
 		{
 			if (Double.parseDouble(value) < Double.parseDouble(parameterValue))
 			{
-				placeHolders = new ArrayList<String>();
-				placeHolders.add(controlCaption);
-				placeHolders.add(parameterValue);
-
-				throw new DynamicExtensionsValidationException("Validation failed", null,
-						"dynExtn.validation.Range.Minimum", placeHolders);
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Minimum");
 			}
 		}
-		else if (parameterName.equals("max"))
+		else if (parameterName.equals(Constants.MAX_VALUE))
 		{
 			if (Double.parseDouble(value) > Double.parseDouble(parameterValue))
 			{
-				placeHolders = new ArrayList<String>();
-				placeHolders.add(controlCaption);
-				placeHolders.add(parameterValue);
-
-				throw new DynamicExtensionsValidationException("Validation failed", null,
-						"dynExtn.validation.Range.Maximum", placeHolders);
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Maximum");
 			}
 		}
+	}
+
+	/**
+	 * @param parameter
+	 * @param controlCaption
+	 * @param value
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void checkFloatValidation(Entry<String, String> parameter, String controlCaption, String value)
+			throws DynamicExtensionsValidationException
+	{
+		String parameterName = parameter.getKey();
+		String parameterValue = parameter.getValue();
+
+		if (parameterName.equals(Constants.MIN_VALUE))
+		{
+			if (Float.parseFloat(value) < Float.parseFloat(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Minimum");
+			}
+		}
+		else if (parameterName.equals(Constants.MAX_VALUE))
+		{
+			if (Float.parseFloat(value) > Float.parseFloat(parameterValue))
+			{
+				reportOutOfRangeInput(controlCaption, parameterValue, "dynExtn.validation.Range.Maximum");
+			}
+		}
+	}
+
+	/**
+	 * @param controlCaption
+	 * @param parameterValue
+	 * @param errorKey
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void reportOutOfRangeInput(String controlCaption, String parameterValue, String errorKey) throws DynamicExtensionsValidationException
+	{
+		List<String> placeHolders = new ArrayList<String>();
+		placeHolders.add(controlCaption);
+		placeHolders.add(parameterValue);
+		throw new DynamicExtensionsValidationException("Validation failed", null, errorKey, placeHolders);
 	}
 
 }

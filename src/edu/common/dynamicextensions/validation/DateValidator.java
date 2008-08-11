201d676ce9b9dd9428d2f8c93e19347745e8c158
mandar_shidhore <mandar_shidhore>
2008-08-11 11:54:11 +0000

    -- Bug 6695    problems with date validation in Annotations forms
    SVN-Revision: 8010

--- src/edu/common/dynamicextensions/validation/DateValidator.java
+++ src/edu/common/dynamicextensions/validation/DateValidator.java
@@ -4,6 +4,7 @@ package edu.common.dynamicextensions.validation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -23,52 +24,114 @@ public class DateValidator implements ValidatorRuleInterface
 	 * @see edu.common.dynamicextensions.validation.ValidatorRuleInterface#validate(edu.common.dynamicextensions.domaininterface.AttributeInterface, java.lang.Object, java.util.Map)
 	 * @throws DynamicExtensionsValidationException
 	 */
-	public boolean validate(AttributeMetadataInterface attribute, Object valueObject,
-			Map<String, String> parameterMap,String controlCaption) throws DynamicExtensionsValidationException
+	public boolean validate(AttributeMetadataInterface attribute, Object valueObject, Map<String, String> parameterMap, String controlCaption) throws DynamicExtensionsValidationException
+	{
+		boolean valid = true;
+		valid = validateDate(attribute, valueObject, parameterMap, controlCaption);
+		
+		return valid;
+	}
+	
+	/**
+	 * Validate user input for permissible date values for date with range. 
+	 * @param attribute
+	 * @param valueObject
+	 * @param parameterMap
+	 * @param controlCaption
+	 * @param isFromDateRangeValidator
+	 * @return
+	 * @throws DynamicExtensionsValidationException
+	 */
+	public boolean validate(AttributeMetadataInterface attribute, Object valueObject, Map<String, String> parameterMap, String controlCaption, boolean isFromDateRangeValidator) throws DynamicExtensionsValidationException
+	{
+		boolean valid = true;
+		valid = validateDate(attribute, valueObject, parameterMap, controlCaption, isFromDateRangeValidator);
+				
+		return valid;
+	}
+
+	/**
+	 * Validate user input for permissible date values.
+	 * @param attribute
+	 * @param valueObject
+	 * @param parameterMap
+	 * @param controlCaption
+	 * @param isFromDateRangeValidator
+	 * @return
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private boolean validateDate(AttributeMetadataInterface attribute, Object valueObject, Map<String, String> parameterMap, String controlCaption, boolean... isFromDateRangeValidator) throws DynamicExtensionsValidationException
 	{
 		boolean valid = true;
 
-		String attributeName = attribute.getName();
-		AttributeTypeInformationInterface attributeTypeInformation = attribute
-				.getAttributeTypeInformation();
+		AttributeTypeInformationInterface attributeTypeInformation = attribute.getAttributeTypeInformation();
 
-		if (((valueObject != null) && (!((String) valueObject).trim().equals("")))
-				&& ((attributeTypeInformation != null) && (attributeTypeInformation instanceof DateAttributeTypeInformation)))
+		if (((valueObject != null) && (!((String) valueObject).trim().equals(""))) && ((attributeTypeInformation != null) && (attributeTypeInformation instanceof DateAttributeTypeInformation)))
 		{
 			DateAttributeTypeInformation dateAttributeTypeInformation = (DateAttributeTypeInformation) attributeTypeInformation;
 			String dateFormat = dateAttributeTypeInformation.getFormat();
 			String value = (String) valueObject;
+			Date tempDate = null;
 
 			try
 			{
 				SimpleDateFormat sf = new SimpleDateFormat(dateFormat);
 				sf.setLenient(false);
-				sf.parse(value);
+				
+				if (isFromDateRangeValidator.length == 0)
+				{
+					tempDate = sf.parse(value);
+				}
+				else
+				{
+					sf.parse(value);
+				}
 			}
 			catch (ParseException parseException)
 			{
 				valid = false;
 			}
-			//Validate if year is equal to '0000' or contains '.' symbol
+			
+			// Validate if year is equal to '0000' or contains '.' symbol
 			if (value.endsWith("0000") || value.contains("."))
 			{
 				valid = false;
 			}
-			//Validate length of entered date
+			// Validate length of entered date
 			if (dateFormat.length() != value.length())
 			{
 				valid = false;
 			}
+			
+			if (valid && isFromDateRangeValidator.length == 0)
+			{
+				if (tempDate.after(new Date()))
+				{
+					reportInvalidInput(controlCaption, "today's date.", "dynExtn.validation.Date.Max");
+				}
+			}
 
 			if (!valid)
 			{
-				List<String> placeHolders = new ArrayList<String>();
-				placeHolders.add(controlCaption);
-				placeHolders.add(dateFormat);
-				throw new DynamicExtensionsValidationException("Validation failed", null,
-						"dynExtn.validation.Date", placeHolders);
+				reportInvalidInput(controlCaption, dateFormat, "dynExtn.validation.Date");
 			}
 		}
+		
 		return valid;
+	}
+
+	/**
+	 * Report invalid user inputs to the user.
+	 * @param placeHolderOne
+	 * @param placeHolderTwo
+	 * @param errorKey
+	 * @throws DynamicExtensionsValidationException
+	 */
+	private void reportInvalidInput(String placeHolderOne, String placeHolderTwo, String errorKey) throws DynamicExtensionsValidationException
+	{
+		List<String> placeHolders = new ArrayList<String>();
+		placeHolders.add(placeHolderOne);
+		placeHolders.add(placeHolderTwo);
+		throw new DynamicExtensionsValidationException("Validation failed", null, errorKey, placeHolders);
 	}
 }

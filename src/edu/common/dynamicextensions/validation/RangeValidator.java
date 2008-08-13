53d9d1982bfc5b8f9ce6217df8c68743a49f640c
falguni_sachde <falguni_sachde>
2008-08-13 10:00:17 +0000

    applying null check for values in case of readonly control
    SVN-Revision: 8050

--- src/edu/common/dynamicextensions/validation/RangeValidator.java
+++ src/edu/common/dynamicextensions/validation/RangeValidator.java
@@ -44,6 +44,8 @@ public class RangeValidator implements ValidatorRuleInterface
 
 		/* Check for the validity of the number */
 		NumberValidator numberValidator = new NumberValidator();
+		//Quick fix 
+		if(valueObject!=null)
 		numberValidator.validate(attribute, valueObject, parameterMap, controlCaption);
 
 		/* Check for the validity of the range of the number against the predefined range*/

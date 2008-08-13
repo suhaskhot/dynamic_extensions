08d8986345070e52251ca6b1c3fbbfb9bf1ebd00
falguni_sachde <falguni_sachde>
2008-08-13 14:43:22 +0000

    applying null check
    SVN-Revision: 8059

--- src/edu/common/dynamicextensions/validation/ValidatorUtil.java
+++ src/edu/common/dynamicextensions/validation/ValidatorUtil.java
@@ -143,7 +143,7 @@ public class ValidatorUtil
 			if (abstractAttribute instanceof AttributeMetadataInterface)
 			{
 				ControlInterface control = getControlOfGivenAbstractAttribute(abstractAttribute, (ContainerInterface) containerInterface);
-
+				if(control!=null)
 				errorList.addAll(validateAttributes(attributeValueNode, control.getCaption()));
 			}
 		}

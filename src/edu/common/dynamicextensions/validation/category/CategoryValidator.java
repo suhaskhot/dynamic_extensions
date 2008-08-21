d4ec80a6182ef0b9ce51c1daeef893709bfcc17b
mandar_shidhore <mandar_shidhore>
2008-08-21 09:36:01 +0000

    -- uses DynamicExtensionsErrorsMessages.properties file for validation messages and checkRequiredRule(....) method added
    SVN-Revision: 8172

--- src/edu/common/dynamicextensions/validation/category/CategoryValidator.java
+++ src/edu/common/dynamicextensions/validation/category/CategoryValidator.java
@@ -8,7 +8,11 @@ import java.util.Map;
 import java.util.Set;
 
 import edu.common.dynamicextensions.domain.DateAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.DoubleAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.FloatAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.IntegerAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.LongAttributeTypeInformation;
+import edu.common.dynamicextensions.domain.ShortAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.StringAttributeTypeInformation;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.common.dynamicextensions.domaininterface.AttributeTypeInformationInterface;
@@ -19,6 +23,7 @@ import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.common.dynamicextensions.processor.ProcessorConstants;
 import edu.common.dynamicextensions.util.parser.CategoryCSVConstants;
 import edu.common.dynamicextensions.util.parser.CategoryCSVFileParser;
+import edu.wustl.common.util.global.ApplicationProperties;
 
 /**
  * @author kunal_kamble
@@ -38,6 +43,7 @@ public class CategoryValidator
 	public CategoryValidator(CategoryCSVFileParser categoryFileParser)
 	{
 		this.categoryFileParser = categoryFileParser;
+		ApplicationProperties.initBundle(CategoryCSVConstants.DYNAMIC_EXTENSIONS_ERROR_MESSAGES_FILE);
 	}
 
 	/**
@@ -63,7 +69,8 @@ public class CategoryValidator
 	{
 		if (categoryFileParser.readLine()[0].split(":").length < 2)
 		{
-			throw new DynamicExtensionsSystemException(getErrorMessageStart() + " miltiplicity not defined.");
+			throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure") + getErrorMessageStart()
+					+ ApplicationProperties.getValue("multiplicityUndefined"));
 		}
 	}
 
@@ -73,7 +80,7 @@ public class CategoryValidator
 	 */
 	public void validateEntityName(String entityName) throws DynamicExtensionsSystemException
 	{
-		String errorMessage = getErrorMessageStart() + "Entity with name " + entityName + " does not exist";
+		String errorMessage = getErrorMessageStart() + ApplicationProperties.getValue("entityDoesNotExist") + entityName;
 		checkForNullRefernce(entityGroup.getEntityByName(entityName), errorMessage);
 	}
 
@@ -94,53 +101,97 @@ public class CategoryValidator
 	{
 		if (object == null)
 		{
-			throw new DynamicExtensionsSystemException(message);
+			throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure") + message);
 		}
 	}
-	
-	public static void checkRangeAgainstAttributeValueRange(AttributeInterface attribute, Map<String, Object> rulesMap) throws DynamicExtensionsSystemException, ParseException
+
+	/**
+	 * @param attribute
+	 * @param rulesMap
+	 * @throws DynamicExtensionsSystemException
+	 * @throws ParseException
+	 */
+	public static void checkRangeAgainstAttributeValueRange(AttributeInterface attribute, Map<String, Object> rulesMap)
+			throws DynamicExtensionsSystemException, ParseException
 	{
 		if (attribute == null)
 		{
-			throw new DynamicExtensionsSystemException("Attribute is NULL");
+			throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+					+ ApplicationProperties.getValue("nullAttribute"));
 		}
-		
-		Map<String, Object> categoryMinMaxValuesMap = (Map<String, Object>) rulesMap.get(CategoryCSVConstants.RANGE.toLowerCase());
-		
-		if (categoryMinMaxValuesMap != null && !categoryMinMaxValuesMap.isEmpty())
+
+		if (!rulesMap.isEmpty())
 		{
-			String minValue = (String) categoryMinMaxValuesMap.get(CategoryCSVConstants.MIN.toLowerCase());
-			String maxValue = (String) categoryMinMaxValuesMap.get(CategoryCSVConstants.MAX.toLowerCase());
-			
-			Set<RuleInterface> rules = new HashSet<RuleInterface>(attribute.getRuleCollection());
-			String min = "";
-			String max = "";
-			
-			for (RuleInterface rule : rules)
+			Map<String, Object> categoryMinMaxValuesMap = (Map<String, Object>) rulesMap.get(CategoryCSVConstants.RANGE.toLowerCase());
+
+			if (categoryMinMaxValuesMap != null && !categoryMinMaxValuesMap.isEmpty())
 			{
-				if (rule.getName().equalsIgnoreCase(CategoryCSVConstants.RANGE) || rule.getName().equalsIgnoreCase("dateRange"))
+				String minValue = (String) categoryMinMaxValuesMap.get(CategoryCSVConstants.MIN.toLowerCase());
+				String maxValue = (String) categoryMinMaxValuesMap.get(CategoryCSVConstants.MAX.toLowerCase());
+
+				Set<RuleInterface> rules = new HashSet<RuleInterface>(attribute.getRuleCollection());
+				String min = "";
+				String max = "";
+
+				for (RuleInterface rule : rules)
 				{
-					Set<RuleParameterInterface> ruleParameters = new HashSet<RuleParameterInterface>(rule.getRuleParameterCollection());
-					
-					for (RuleParameterInterface ruleParameter : ruleParameters)
+					if (rule.getName().equalsIgnoreCase(CategoryCSVConstants.RANGE) || rule.getName().equalsIgnoreCase(ProcessorConstants.DATE_RANGE))
 					{
-						if (ruleParameter.getName().equalsIgnoreCase(CategoryCSVConstants.MIN))
+						Set<RuleParameterInterface> ruleParameters = new HashSet<RuleParameterInterface>(rule.getRuleParameterCollection());
+
+						for (RuleParameterInterface ruleParameter : ruleParameters)
 						{
-							min = ruleParameter.getValue();
-						}
-						if (ruleParameter.getName().equalsIgnoreCase(CategoryCSVConstants.MAX))
-						{
-							max = ruleParameter.getValue();
+							if (ruleParameter.getName().equalsIgnoreCase(CategoryCSVConstants.MIN))
+							{
+								min = ruleParameter.getValue();
+							}
+							if (ruleParameter.getName().equalsIgnoreCase(CategoryCSVConstants.MAX))
+							{
+								max = ruleParameter.getValue();
+							}
 						}
 					}
 				}
-			}
-			
-			if (min.length() != 0 && max.length() != 0)
-			{
-				validateRange(attribute.getAttributeTypeInformation(), minValue, maxValue, min, max);
+
+				if (min.length() != 0 && max.length() != 0)
+				{
+					validateRange(attribute.getAttributeTypeInformation(), minValue, maxValue, min, max);
+				}
 			}
 		}
+	}
+
+	/**
+	 * @param attribute
+	 * @param rulesMap
+	 * @throws DynamicExtensionsSystemException
+	 * @throws ParseException
+	 */
+	public static void checkRequiredRule(AttributeInterface attribute, Map<String, Object> rulesMap) throws DynamicExtensionsSystemException,
+			ParseException
+	{
+		if (attribute == null)
+		{
+			throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+					+ ApplicationProperties.getValue("nullAttribute"));
+		}
+
+		for (RuleInterface rule : attribute.getRuleCollection())
+		{
+			if (rule.getName().equalsIgnoreCase(CategoryCSVConstants.REQUIRED) && (rulesMap == null || rulesMap.isEmpty()))
+			{
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("cannotOverrideRequiredRule") + attribute.getName());
+			}
+		}
+	}
+
+	/**
+	 * @return
+	 */
+	private String getErrorMessageStart()
+	{
+		return ApplicationProperties.getValue("lineNumber") + categoryFileParser.getLineNumber() + " ";
 	}
 
 	/**
@@ -152,32 +203,59 @@ public class CategoryValidator
 	 * @throws DynamicExtensionsSystemException
 	 * @throws ParseException
 	 */
-	private static void validateRange(AttributeTypeInformationInterface attributeTypeInformation, String minValue, String maxValue, String min, String max) throws DynamicExtensionsSystemException, ParseException
+	private static void validateRange(AttributeTypeInformationInterface attributeTypeInformation, String minValue, String maxValue, String min,
+			String max) throws DynamicExtensionsSystemException, ParseException
 	{
 		if (attributeTypeInformation instanceof IntegerAttributeTypeInformation || attributeTypeInformation instanceof StringAttributeTypeInformation)
 		{
 			if (Integer.parseInt(minValue) < Integer.parseInt(min) || Integer.parseInt(maxValue) > Integer.parseInt(max))
 			{
-				throw new DynamicExtensionsSystemException("Range values for category attributes must be a subset of range values for its attribute."); 
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("numberRangeError"));
+			}
+		}
+		else if (attributeTypeInformation instanceof FloatAttributeTypeInformation)
+		{
+			if (Float.parseFloat(minValue) < Float.parseFloat(min) || Float.parseFloat(maxValue) > Float.parseFloat(max))
+			{
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("numberRangeError"));
+			}
+		}
+		else if (attributeTypeInformation instanceof ShortAttributeTypeInformation)
+		{
+			if (Short.parseShort(minValue) < Short.parseShort(min) || Short.parseShort(maxValue) > Short.parseShort(max))
+			{
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("numberRangeError"));
+			}
+		}
+		else if (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
+		{
+			if (Double.parseDouble(minValue) < Double.parseDouble(min) || Double.parseDouble(maxValue) > Double.parseDouble(max))
+			{
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("numberRangeError"));
+			}
+		}
+		else if (attributeTypeInformation instanceof LongAttributeTypeInformation)
+		{
+			if (Long.parseLong(minValue) < Long.parseLong(min) || Long.parseLong(maxValue) > Long.parseLong(max))
+			{
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("numberRangeError"));
 			}
 		}
 		else if (attributeTypeInformation instanceof DateAttributeTypeInformation)
-		{		
+		{
 			SimpleDateFormat sf = new SimpleDateFormat(ProcessorConstants.SQL_DATE_ONLY_FORMAT);
 			sf.setLenient(false);
 			if (sf.parse(minValue).before(sf.parse(min)) || sf.parse(maxValue).after(sf.parse(max)))
 			{
-				throw new DynamicExtensionsSystemException("Date range values for category attributes must be a subset of date range values for its attribute."); 
+				throw new DynamicExtensionsSystemException(ApplicationProperties.getValue("categoryCreationFailure")
+						+ ApplicationProperties.getValue("dateRangeError"));
 			}
 		}
-	}
-
-	/**
-	 * @return
-	 */
-	private String getErrorMessageStart()
-	{
-		return "Error at line:" + categoryFileParser.getLineNumber() + " ";
 	}
 
 }

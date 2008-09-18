9d2262d0dea8511cc993618ab493a283ae215976
mandar_shidhore <mandar_shidhore>
2008-09-18 11:45:24 +0000

    -- get the default permissible value for date type category attribute
    SVN-Revision: 8828

--- src/edu/common/dynamicextensions/domain/DateAttributeTypeInformation.java
+++ src/edu/common/dynamicextensions/domain/DateAttributeTypeInformation.java
@@ -18,45 +18,43 @@ import edu.wustl.common.util.Utility;
  * @hibernate.joined-subclass table="DYEXTN_DATE_TYPE_INFO"
  * @hibernate.joined-subclass-key column="IDENTIFIER"
  */
-public class DateAttributeTypeInformation extends AttributeTypeInformation
-        implements
-            DateTypeInformationInterface
+public class DateAttributeTypeInformation extends AttributeTypeInformation implements DateTypeInformationInterface
 {
 
-    /**
-     * Serial Version Unique Identifier
-     */
-    private static final long serialVersionUID = 5655678242696814276L;
+	/**
+	 * Serial Version Unique Identifier
+	 */
+	private static final long serialVersionUID = 5655678242696814276L;
 
-    /**
-     * format of the attribute value (Data entry/display)
-     */
-    protected String format;
+	/**
+	 * format of the attribute value (Data entry/display)
+	 */
+	protected String format;
 
-    /**
-     * Empty Constructor
-     */
-    public DateAttributeTypeInformation()
-    {
+	/**
+	 * Empty Constructor
+	 */
+	public DateAttributeTypeInformation()
+	{
 
-    }
+	}
 
-    /**This method returns the format of the DateAttributeTypeInformation.
-     * @hibernate.property name="format" type="string" column="FORMAT"
-     * @return Returns the format.
-     */
-    public String getFormat()
-    {
-        return format;
-    }
+	/**This method returns the format of the DateAttributeTypeInformation.
+	 * @hibernate.property name="format" type="string" column="FORMAT"
+	 * @return Returns the format.
+	 */
+	public String getFormat()
+	{
+		return format;
+	}
 
-    /**
-     * @param format The format to set.
-     */
-    public void setFormat(String format)
-    {
-        this.format = format;
-    }
+	/**
+	 * @param format The format to set.
+	 */
+	public void setFormat(String format)
+	{
+		this.format = format;
+	}
 
 	/**
 	 * @see edu.common.dynamicextensions.domaininterface.AttributeTypeInformationInterface#getDataType()
@@ -66,28 +64,49 @@ public class DateAttributeTypeInformation extends AttributeTypeInformation
 
 		return EntityManagerConstantsInterface.DATE_ATTRIBUTE_TYPE;
 	}
-	
+
 	/**
 	 * 
 	 */
-	public PermissibleValueInterface getPermissibleValueForString(String value) throws ParseException {
-		DomainObjectFactory domainObjectFactory = DomainObjectFactory.getInstance();
-		DateValueInterface dateValueInterface = domainObjectFactory.createDateValue();
-		
-		if (format.equals(ProcessorConstants.MONTH_YEAR_FORMAT))
-        {
-            value = DynamicExtensionsUtility.formatMonthAndYearDate(value);
-            value = value.substring(0, value.length()-4);
-        }
-        if (format.equals(ProcessorConstants.YEAR_ONLY_FORMAT))
-        {
-            value = DynamicExtensionsUtility.formatYearDate(value);
-            value = value.substring(0, value.length()-4);
-        }
+	public PermissibleValueInterface getPermissibleValueForString(String value) throws ParseException
+	{
+		value = value.replace("/", "-");
 
-		Date date = Utility.parseDate(value, "MM-dd-yyyy");
-		dateValueInterface.setValue(date);
-		return dateValueInterface ;
+		if (value.length() != format.length())
+		{
+			throw new ParseException("DATE VALUE " + value + " INVALID FOR " + format + " DATE FORMAT.", 0);
+		}
+
+		if (value.endsWith("0000") || value.contains("."))
+		{
+			throw new ParseException("DATE VALUE " + value + " INVALID FOR " + format + " DATE FORMAT.", 0);
+		}
+
+		DateValueInterface dateValue = DomainObjectFactory.getInstance().createDateValue();
+
+		if (format.equals(ProcessorConstants.MONTH_YEAR_FORMAT))
+		{
+			Utility.parseDate(value, format);
+			value = DynamicExtensionsUtility.formatMonthAndYearDate(value);
+		}
+		if (format.equals(ProcessorConstants.YEAR_ONLY_FORMAT))
+		{
+			Utility.parseDate(value, format);
+			value = DynamicExtensionsUtility.formatYearDate(value);
+		}
+
+		Date date = null;
+		if (format.equals(ProcessorConstants.DATE_TIME_FORMAT))
+		{
+			date = Utility.parseDate(value, ProcessorConstants.DATE_TIME_FORMAT);
+		}
+		else
+		{
+			date = Utility.parseDate(value, ProcessorConstants.SQL_DATE_ONLY_FORMAT);
+		}
+
+		dateValue.setValue(date);
+		return dateValue;
 	}
 
 }

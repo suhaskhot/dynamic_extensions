6dd687876a647a6eb90014669c5676ec1756fac0
mandar_shidhore <mandar_shidhore>
2008-09-18 12:02:55 +0000

    -- A class where constants are defined for category creation error messages.
    SVN-Revision: 8834

---
+++ src/edu/common/dynamicextensions/util/global/CategoryConstants.java
@@ -0,0 +1,34 @@
+
+package edu.common.dynamicextensions.util.global;
+
+/**
+ * A class where constants are defined for category creation error messages.
+ * @author mandar_shidhore
+ */
+public class CategoryConstants
+{
+	public final static String CREATE_CAT_FAILS = "categoryCreationFailure";
+
+	public final static String CONTACT_ADMIN = "contactAdmin";
+
+	public final static String MULT_UNDEFINED = "multiplicityUndefined";
+
+	public final static String NO_ENTITY = "entityDoesNotExist";
+
+	public final static String NULL_ATTR = "nullAttribute";
+
+	public final static String NULL_ATTR_TYPE_INFO = "nullAttributeTypeInformation";
+
+	public final static String NON_NUM_RANGE = "rangeForNonNumeric";
+
+	public final static String NO_OVERRIDE_REQ_RULE = "cannotOverrideRequiredRule";
+
+	public final static String LINE_NUMBER = "lineNumber";
+
+	public final static String ROOT_ENT_TWICE = "rootEntityUsedTwice";
+
+	public final static String NO_TEXTAREA = "incorrectControlTypeTextArea";
+
+	public final static String WRONG_INST_INFO = "wrongInstanceInformation";
+
+}

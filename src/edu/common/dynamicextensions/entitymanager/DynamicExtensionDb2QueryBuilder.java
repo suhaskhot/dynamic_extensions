2be79c1c96003f5a0535cb33f31751682c10e291
mandar_shidhore <mandar_shidhore>
2008-11-10 09:31:35 +0000

    -- unused imports removed + code formatter applied + asterix keyword taken from DynamicExtensionsQueryBuilderConstantsInterface.java
    SVN-Revision: 10058

--- src/edu/common/dynamicextensions/entitymanager/DynamicExtensionDb2QueryBuilder.java
+++ src/edu/common/dynamicextensions/entitymanager/DynamicExtensionDb2QueryBuilder.java
@@ -1,3 +1,4 @@
+
 package edu.common.dynamicextensions.entitymanager;
 
 import java.sql.ResultSet;
@@ -12,14 +13,8 @@ import edu.common.dynamicextensions.domain.Attribute;
 import edu.common.dynamicextensions.domain.BooleanAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.DateAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.DomainObjectFactory;
-import edu.common.dynamicextensions.domain.DoubleAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.FileAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.FloatAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.IntegerAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.LongAttributeTypeInformation;
-import edu.common.dynamicextensions.domain.ShortAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.StringAttributeTypeInformation;
-import edu.common.dynamicextensions.domaininterface.AbstractEntityInterface;
 import edu.common.dynamicextensions.domaininterface.AttributeTypeInformationInterface;
 import edu.common.dynamicextensions.domaininterface.CategoryAttributeInterface;
 import edu.common.dynamicextensions.exception.DataTypeFactoryInitializationException;
@@ -39,62 +34,67 @@ import edu.wustl.common.util.logger.Logger;
  */
 public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBuilder
 {
+
 	/**
 	 * This method builds the query part for the primitive attribute
 	 * @param attribute primitive attribute for which to build the query.
 	 * @return String query part of the primitive attribute.
 	 * @throws DataTypeFactoryInitializationException
 	 */
-	protected String getQueryPartForAttribute(Attribute attribute, String type, boolean processConstraints) throws DynamicExtensionsSystemException
+	protected String getQueryPartForAttribute(Attribute attribute, String type,
+			boolean processConstraints) throws DynamicExtensionsSystemException
 	{
 
 		String attributeQuery = null;
 		if (attribute != null)
 		{
-			
+
 			String columnName = attribute.getColumnProperties().getName();
 			String nullConstraint = "";
-					// Dead code removed because no need to keep it there.
-			
+			// Dead code removed because no need to keep it there.
+
 			if (processConstraints && !attribute.getIsNullable())
 			{
 				nullConstraint = "NOT NULL";
 			}
 
-			attributeQuery = columnName + WHITESPACE + type + WHITESPACE + getDatabaseTypeAndSize(attribute) //+ WHITESPACE + defaultConstraint
+			attributeQuery = columnName + WHITESPACE + type + WHITESPACE
+					+ getDatabaseTypeAndSize(attribute) //+ WHITESPACE + defaultConstraint
 					+ WHITESPACE + nullConstraint;
 		}
 		return attributeQuery;
 	}
-	
+
 	/**
 	 * This method builds the query part for the primitive attribute
 	 * @param attribute primitive attribute for which to build the query.
 	 * @return String query part of the primitive attribute.
 	 * @throws DataTypeFactoryInitializationException
 	 */
-	protected String getQueryPartForCategoryAttribute(CategoryAttributeInterface attribute, String type, boolean processConstraints)
-	throws DynamicExtensionsSystemException
+	protected String getQueryPartForCategoryAttribute(CategoryAttributeInterface attribute,
+			String type, boolean processConstraints) throws DynamicExtensionsSystemException
 	{
 		String attributeQuery = null;
 		if (attribute != null)
 		{
 			String columnName = attribute.getColumnProperties().getName();
 			String nullConstraint = "";
-					//dead code Removed
-			attributeQuery = columnName + WHITESPACE + type + WHITESPACE + getDataTypeForIdentifier() + WHITESPACE + nullConstraint;
+			//dead code Removed
+			attributeQuery = columnName + WHITESPACE + type + WHITESPACE
+					+ getDataTypeForIdentifier() + WHITESPACE + nullConstraint;
 		}
 		return attributeQuery;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public Object getFormattedValue(AbstractAttribute attribute, Object value)
 	{
-		
+
 		String formattedvalue = null;
-		AttributeTypeInformationInterface attributeInformation = ((Attribute) attribute).getAttributeTypeInformation();
+		AttributeTypeInformationInterface attributeInformation = ((Attribute) attribute)
+				.getAttributeTypeInformation();
 		if (attribute == null)
 		{
 			formattedvalue = null;
@@ -107,11 +107,14 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 			{
 				if (((List) value).size() > 0)
 				{
-					formattedvalue = "'" + DynamicExtensionsUtility.getEscapedStringValue((String) ((List) value).get(0)) + "'";
+					formattedvalue = "'"
+							+ DynamicExtensionsUtility
+									.getEscapedStringValue((String) ((List) value).get(0)) + "'";
 				}
 			}
 			else
-				formattedvalue = "'" + DynamicExtensionsUtility.getEscapedStringValue((String) value) + "'";
+				formattedvalue = "'"
+						+ DynamicExtensionsUtility.getEscapedStringValue((String) value) + "'";
 		}
 		else if (attributeInformation instanceof DateAttributeTypeInformation)
 		{
@@ -136,7 +139,7 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 				if (str.length() != 0)
 				{
 					str = DynamicExtensionsUtility.formatMonthAndYearDate(str);
-					
+
 				}
 			}
 
@@ -145,7 +148,7 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 				if (str.length() != 0)
 				{
 					str = DynamicExtensionsUtility.formatYearDate(str);
-					
+
 				}
 			}
 			// if user not enter any value for date field its getting saved as 00-00-0000 ,which is throwing exception
@@ -157,8 +160,8 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 			}
 			else
 			{
-				formattedvalue = Variables.strTodateFunction + "('" + str.trim() + "','" + DynamicExtensionsUtility.getSQLDateFormat(dateFormat)
-						+ "')";
+				formattedvalue = Variables.strTodateFunction + "('" + str.trim() + "','"
+						+ DynamicExtensionsUtility.getSQLDateFormat(dateFormat) + "')";
 			}
 		}
 		else
@@ -189,31 +192,35 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 				{
 					formattedvalue = null;
 				}
-				
+
 			}
-			
+
 		}
-		Logger.out.debug("getFormattedValue The formatted value for attribute " + attribute.getName() + "is " + formattedvalue);
+		Logger.out.debug("getFormattedValue The formatted value for attribute "
+				+ attribute.getName() + "is " + formattedvalue);
 		return formattedvalue;
 	}
-	
-	
+
 	/**
 	 * @param tableName
 	 * @param columnName
 	 * @return
 	 * @throws DynamicExtensionsSystemException
 	 */
-	public boolean isDataPresent(String tableName, Attribute savedAttribute) throws DynamicExtensionsSystemException
+	public boolean isDataPresent(String tableName, Attribute savedAttribute)
+			throws DynamicExtensionsSystemException
 	{
 		boolean dataPresent = false;
 		StringBuffer queryBuffer = new StringBuffer();
 		if (!isAttributeColumnToBeExcluded(savedAttribute))
 		{
-			queryBuffer.append(SELECT_KEYWORD).append(WHITESPACE).append("COUNT").append(OPENING_BRACKET).append("*").append(CLOSING_BRACKET).append(
-					WHITESPACE).append(FROM_KEYWORD).append(WHITESPACE).append(tableName).append(WHITESPACE).append(WHERE_KEYWORD).append(WHITESPACE)
-					.append(savedAttribute.getColumnProperties().getName()).append(WHITESPACE).append("IS").append(WHITESPACE).append(NOT_KEYWORD)
-					.append(WHITESPACE).append(NULL_KEYWORD);
+			queryBuffer.append(SELECT_KEYWORD).append(WHITESPACE).append("COUNT").append(
+					OPENING_BRACKET).append(ASTERIX).append(CLOSING_BRACKET).append(WHITESPACE)
+					.append(FROM_KEYWORD).append(WHITESPACE).append(tableName).append(WHITESPACE)
+					.append(WHERE_KEYWORD).append(WHITESPACE).append(
+							savedAttribute.getColumnProperties().getName()).append(WHITESPACE)
+					.append("IS").append(WHITESPACE).append(NOT_KEYWORD).append(WHITESPACE).append(
+							NULL_KEYWORD);
 			System.out.println(queryBuffer);
 			ResultSet resultSet = null;
 			try
@@ -228,7 +235,8 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 			}
 			catch (SQLException e)
 			{
-				throw new DynamicExtensionsSystemException("Can not check the availability of data", e);
+				throw new DynamicExtensionsSystemException(
+						"Can not check the availability of data", e);
 			}
 			finally
 			{
@@ -247,8 +255,9 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 		}
 		else
 		{
-			Collection<Integer> recordCollection = EntityManager.getInstance().getAttributeRecordsCount(savedAttribute.getEntity().getId(),
-					savedAttribute.getId());
+			Collection<Integer> recordCollection = EntityManager.getInstance()
+					.getAttributeRecordsCount(savedAttribute.getEntity().getId(),
+							savedAttribute.getId());
 			if (recordCollection != null && !recordCollection.isEmpty())
 			{
 				Integer count = (Integer) recordCollection.iterator().next();
@@ -260,7 +269,7 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 		}
 		return dataPresent;
 	}
-	
+
 	/**
 	 * This method returns the query for the attribute to modify its data type.
 	 * @param attribute
@@ -270,7 +279,8 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 	 * @return
 	 * @throws DynamicExtensionsSystemException
 	 */
-	protected List<String> getAttributeDataTypeChangedQuery(Attribute attribute, Attribute savedAttribute, List modifyAttributeRollbackQueryList)
+	protected List<String> getAttributeDataTypeChangedQuery(Attribute attribute,
+			Attribute savedAttribute, List modifyAttributeRollbackQueryList)
 			throws DynamicExtensionsSystemException
 	{
 		String tableName = attribute.getEntity().getTableProperties().getName();
@@ -278,20 +288,23 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 		String modifyAttributeRollbackQuery = "";
 
 		String modifyAttributeQuery = getQueryPartForAttribute(attribute, type, false);
-		modifyAttributeQuery = ALTER_TABLE + tableName + ADD_KEYWORD +  modifyAttributeQuery;
+		modifyAttributeQuery = ALTER_TABLE + tableName + ADD_KEYWORD + modifyAttributeQuery;
 
 		modifyAttributeRollbackQuery = getQueryPartForAttribute(savedAttribute, type, false);
-		modifyAttributeRollbackQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE + MODIFY_KEYWORD + WHITESPACE + modifyAttributeRollbackQuery;
+		modifyAttributeRollbackQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE
+				+ MODIFY_KEYWORD + WHITESPACE + modifyAttributeRollbackQuery;
 
 		String nullQueryKeyword = "";
 		String nullQueryRollbackKeyword = "";
 		List<String> modifyAttributeQueryList = new ArrayList<String>();
-		modifyAttributeQueryList.add(ALTER_TABLE + tableName + DROP_KEYWORD + COLUMN_KEYWORD + savedAttribute.getColumnProperties().getName());
+		modifyAttributeQueryList.add(ALTER_TABLE + tableName + DROP_KEYWORD + COLUMN_KEYWORD
+				+ savedAttribute.getColumnProperties().getName());
 
 		if (attribute.getIsNullable() && !savedAttribute.getIsNullable())
 		{
 			nullQueryKeyword = WHITESPACE + NULL_KEYWORD + WHITESPACE;
-			nullQueryRollbackKeyword = WHITESPACE + NOT_KEYWORD + WHITESPACE + NULL_KEYWORD + WHITESPACE;
+			nullQueryRollbackKeyword = WHITESPACE + NOT_KEYWORD + WHITESPACE + NULL_KEYWORD
+					+ WHITESPACE;
 		}
 		else if (!attribute.getIsNullable() && savedAttribute.getIsNullable())
 		{
@@ -301,21 +314,22 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 		}
 		if (attribute.getAttributeTypeInformation() instanceof FileAttributeTypeInformation)
 		{
-			
-			modifyAttributeQuery = modifyAttributeQuery +  extraColumnQueryStringForFileAttribute(attribute);
-			modifyAttributeRollbackQuery = modifyAttributeRollbackQuery + dropExtraColumnQueryStringForFileAttribute(attribute);
+
+			modifyAttributeQuery = modifyAttributeQuery
+					+ extraColumnQueryStringForFileAttribute(attribute);
+			modifyAttributeRollbackQuery = modifyAttributeRollbackQuery
+					+ dropExtraColumnQueryStringForFileAttribute(attribute);
 
 		}
 		modifyAttributeQuery = modifyAttributeQuery + nullQueryKeyword;
 		modifyAttributeRollbackQuery = modifyAttributeRollbackQuery + nullQueryRollbackKeyword;
 		modifyAttributeRollbackQueryList.add(modifyAttributeRollbackQuery);
 
-		
 		modifyAttributeQueryList.add(modifyAttributeQuery);
 
 		return modifyAttributeQueryList;
 	}
-	
+
 	/**
 	 * This method contrsucts the query part for adding two extra columns when
 	 * an attribute of type File is created in existing entity
@@ -323,14 +337,18 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 	 * @return queryString
 	 * @throws DynamicExtensionsSystemException
 	 */
-	private String extraColumnQueryStringForFileAttribute(Attribute attribute) throws DynamicExtensionsSystemException
+	private String extraColumnQueryStringForFileAttribute(Attribute attribute)
+			throws DynamicExtensionsSystemException
 	{
-		Attribute stringAttribute = (Attribute) DomainObjectFactory.getInstance().createStringAttribute();
-		String queryString = ADD_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE + FILE_NAME + WHITESPACE + getDatabaseTypeAndSize(stringAttribute)	+ WHITESPACE 
-		+ ADD_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE + CONTENT_TYPE + WHITESPACE + getDatabaseTypeAndSize(stringAttribute);
+		Attribute stringAttribute = (Attribute) DomainObjectFactory.getInstance()
+				.createStringAttribute();
+		String queryString = ADD_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE
+				+ FILE_NAME + WHITESPACE + getDatabaseTypeAndSize(stringAttribute) + WHITESPACE
+				+ ADD_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE + CONTENT_TYPE
+				+ WHITESPACE + getDatabaseTypeAndSize(stringAttribute);
 		return queryString;
 	}
-	
+
 	/**
 	 * This method constructs the query part for dropping the extra columns
 	 * created while creating an attribute of type File
@@ -338,10 +356,13 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 	 * @return queryString
 	 * @throws DynamicExtensionsSystemException
 	 */
-	
-	private String dropExtraColumnQueryStringForFileAttribute(Attribute attribute) throws DynamicExtensionsSystemException
+
+	private String dropExtraColumnQueryStringForFileAttribute(Attribute attribute)
+			throws DynamicExtensionsSystemException
 	{
-		String queryString = DROP_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE + FILE_NAME + WHITESPACE + DROP_KEYWORD + attribute.getName() + UNDERSCORE + CONTENT_TYPE;
+		String queryString = DROP_KEYWORD + WHITESPACE + attribute.getName() + UNDERSCORE
+				+ FILE_NAME + WHITESPACE + DROP_KEYWORD + attribute.getName() + UNDERSCORE
+				+ CONTENT_TYPE;
 		return queryString;
 	}
 
@@ -353,26 +374,26 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 	 * @throws DynamicExtensionsSystemException
 	 * @throws DynamicExtensionsApplicationException
 	 */
-	protected String processAddAttribute(Attribute attribute, List attributeRollbackQueryList) throws DynamicExtensionsSystemException,
-			DynamicExtensionsApplicationException
+	protected String processAddAttribute(Attribute attribute, List attributeRollbackQueryList)
+			throws DynamicExtensionsSystemException, DynamicExtensionsApplicationException
 	{
 
 		String columnName = attribute.getColumnProperties().getName();
 		String tableName = attribute.getEntity().getTableProperties().getName();
 		String type = "";
-		String newAttributeQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE + ADD_KEYWORD + WHITESPACE 
-				+ getQueryPartForAttribute(attribute, type, true);
+		String newAttributeQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE + ADD_KEYWORD
+				+ WHITESPACE + getQueryPartForAttribute(attribute, type, true);
 
-		String newAttributeRollbackQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE + DROP_KEYWORD + WHITESPACE + COLUMN_KEYWORD
-				+ WHITESPACE  + columnName;
+		String newAttributeRollbackQuery = ALTER_TABLE + WHITESPACE + tableName + WHITESPACE
+				+ DROP_KEYWORD + WHITESPACE + COLUMN_KEYWORD + WHITESPACE + columnName;
 		if (attribute.getAttributeTypeInformation() instanceof FileAttributeTypeInformation)
 		{
-			
-			newAttributeQuery +=extraColumnQueryStringForFileAttribute(attribute);
+
+			newAttributeQuery += extraColumnQueryStringForFileAttribute(attribute);
 			newAttributeRollbackQuery += dropExtraColumnQueryStringForFileAttribute(attribute);
 
 		}
-		
+
 		attributeRollbackQueryList.add(newAttributeRollbackQuery);
 
 		return newAttributeQuery;
@@ -405,5 +426,5 @@ public class DynamicExtensionDb2QueryBuilder extends DynamicExtensionBaseQueryBu
 
 		return alterTableQuery.toString();
 	}
-	
+
 }

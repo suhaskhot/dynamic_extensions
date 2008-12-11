ad6261f26d7c3af36657bfae2c20a84091de9a7c
mandar_shidhore <mandar_shidhore>
2008-12-11 06:21:08 +0000

    -- Bug 10637 It should be possible to add just a heading/label anywhere in the form
    SVN-Revision: 11300

---
+++ src/edu/common/dynamicextensions/domaininterface/FormControlNotesInterface.java
@@ -0,0 +1,14 @@
+package edu.common.dynamicextensions.domaininterface;
+
+/**
+ * 
+ * @author mandar_shidhore
+ *
+ */
+public interface FormControlNotesInterface
+{
+	public String getNote();
+	
+	public void setNote(String note);
+
+}

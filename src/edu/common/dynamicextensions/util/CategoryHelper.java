476905c567bee5c56e2746db130d7c41de7273aa
rajesh_patil <rajesh_patil>
2008-12-24 13:44:48 +0000

    Fixed 5551- Added PermValueOptions in category file
    SVN-Revision: 11718

--- src/edu/common/dynamicextensions/util/CategoryHelper.java
+++ src/edu/common/dynamicextensions/util/CategoryHelper.java
@@ -1145,6 +1145,8 @@ public class CategoryHelper implements CategoryHelperInterface
 
 		((CategoryAttribute) baseAbstractAttribute).setDataElement(userDefinedDE);
 
+		setOptions(userDefinedDE, permValueOptions, lineNumber);
+
 		return radioButton;
 	}
 

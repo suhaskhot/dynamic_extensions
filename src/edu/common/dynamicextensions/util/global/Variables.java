bd06c6db962a400a5db485cb97463e6cdcae5eb2
suhas_khot <suhas_khot>
2009-02-19 13:34:45 +0000

    fixed pmd:StringInstantiation
    SVN-Revision: 15502

--- src/edu/common/dynamicextensions/util/global/Variables.java
+++ src/edu/common/dynamicextensions/util/global/Variables.java
@@ -19,11 +19,11 @@ import java.util.List;
 public class Variables
 {
 
-	public static String dynamicExtensionsHome = new String();
+	public static String dynamicExtensionsHome = "";
 	public static List databaseDefinitions = new ArrayList();	
-	public static String databaseDriver = new String();
+	public static String databaseDriver = "";
 	public static String[] databasenames;
 	public static boolean containerFlag = true;
-	public static String applicationCvsTag = new String();
+	public static String applicationCvsTag = "";
 	public static String hibernateCfgFileName = "hibernate.cfg.xml";
 }

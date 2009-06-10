a6e21c143bf34df3b4e3cab2158d9d0f38cb9fc2
pavan_kalantri <pavan_kalantri>
2009-06-10 12:25:23 +0000

    removed pmd
    SVN-Revision: 21094

--- src/edu/wustl/cab2b/server/util/DynamicExtensionUtility.java
+++ src/edu/wustl/cab2b/server/util/DynamicExtensionUtility.java
@@ -52,7 +52,6 @@ import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
 import edu.wustl.cab2b.common.exception.RuntimeException;
 import edu.wustl.cab2b.common.util.Utility;
 import edu.wustl.cab2b.server.cache.EntityCache;
-import edu.wustl.cab2b.server.path.PathConstants;
 import edu.wustl.common.bizlogic.AbstractBizLogic;
 import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.querysuite.queryobject.DataType;

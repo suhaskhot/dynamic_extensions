cecbe24bbdfc62b3443da43c6e2cc9ca07bb0a2d
mandar_shidhore <mandar_shidhore>
2009-07-30 10:55:34 +0000

    -- Bug 13512 : For forms, it should be possible to optionally use multiselect with autocomplete drop down
    SVN-Revision: 23605

--- de/jss/ajax.js
+++ de/jss/ajax.js
@@ -67,3 +67,146 @@ function getReadyStateHandler(request,responseXmlHandler,isText)
     }
 }
 
+// Functions to implement multiselect list box using autcomplete combobox start here.
+function moveOptions(theSelFromId, theSelToId, operation) 
+{
+	addFromList(theSelFromId, theSelToId, operation);
+
+	var theSelFrom = document.getElementById(theSelFromId);
+	var theSelTo = document.getElementById(theSelToId);    
+	var selLength = theSelFrom.length;
+	var selectedText = new Array();
+	var selectedValues = new Array();
+	var selectedCount = 0;
+	var i;
+
+	// Find the selected Options in reverse order
+	// and delete them from the 'from' Select.
+
+	for (i = selLength - 1; i >= 0; i--)
+	{
+		if (theSelFrom.options[i].selected)
+		{
+			selectedText[selectedCount] = theSelFrom.options[i].text;
+			selectedValues[selectedCount] = theSelFrom.options[i].value;
+			selectedCount++;
+
+			deleteOption(theSelFrom, i);
+		}
+	}
+
+	// Add the selected text/values in reverse order.
+	// This will add the Options to the 'to' Select
+	// in the same order as they were in the 'from' Select.
+
+	if (operation != 'edit')
+	{
+		for (i = selectedCount - 1; i >= 0; i--)
+		{
+			addOption(theSelTo, selectedText[i], selectedValues[i]);
+		}
+	}
+}
+
+function addFromList(theSelFromId, theSelToId, operation)
+{
+	var getListText = document.getElementById(theSelFromId).value;
+	var getListValue = null;
+
+	if (document.getElementById(("CB_"+theSelFromId)) != null)
+	{
+		getListValue = document.getElementById(("CB_"+theSelFromId)).value;
+	}
+	
+	var theSel = document.getElementById(theSelToId);
+	var selLength = theSel.length;
+	
+	var exists = false;
+	for ( var i = 0; i < selLength; i++) 
+	{
+		if (theSel.options[i].value == getListValue) 
+		{
+			exists = true;
+			break;
+		}
+	}
+
+	if (!exists)
+	{
+		if (operation != 'edit' && getListText != null && (getListText == "--Select--" || getListText == "-- Select --"))
+		{
+			alert("--Select-- is not a valid coordinator name. Please choose another.");
+			return;
+		}
+
+		if (getListText != null && getListText != "" && getListValue!= null && getListValue!= "")
+		{
+			if (theSelFromId != 'protocolCoordinatorIds')
+			{
+				addOption(theSel,getListText,getListValue);
+			}
+		}
+	}
+	
+	return;
+}
+
+function deleteOption(theSel, theIndex) 
+{
+	var selLength = theSel.length;
+
+	if (selLength > 0) 
+	{
+		//theSel.options[theIndex].title="";
+		theSel.options[theIndex] = null;
+	}
+}
+
+
+function addOption(theSel, theText, theValue) 
+{
+	var newOpt = new Option(theText, theValue);
+	var selLength = theSel.length;
+	var exists = "false";
+	for ( var i = 0; i < selLength; i++) 
+	{
+		if (theSel.options[i].value == theValue) 
+		{
+			exists = "true";
+			break;
+		}
+	}
+	
+	if (exists == "false")
+	{
+		theSel.options[selLength] = newOpt;
+		theSel.options[selLength].title=theText;
+	}
+}
+
+function selectCoordinators(formname)
+{
+	var coords = document.getElementById('protocolCoordinatorIds');
+	if (coords != null)
+	{
+		for (i = coords.options.length-1; i >= 0; i--)
+		{
+			coords.options[i].selected=true;
+		}
+	}
+
+	checkForDuplicates(formname);
+}
+
+function selectPresentCoordinators()
+{
+	var coords = document.getElementById('protocolCoordinatorIds');
+	if (coords != null)
+	{
+		for (i = coords.options.length-1; i >= 0; i--)
+		{
+			coords.options[i].selected=true;
+		}
+	}
+}
+//Functions to implement multiselect list box using autcomplete combobox end here.

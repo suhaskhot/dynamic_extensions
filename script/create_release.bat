1d7cf29a95fee9699dfa3cad48c173f62a75a0ff
pulkit_kumar <pulkit_kumar>
2009-09-26 03:57:27 +0000

    SVN-Revision: 25638

--- script/create_release.bat
+++
@@ -1,9 +0,0 @@
-cd ./../temp/project
-
-echo ===============================================================================
-
-call ant create_dynamic_extensions_zip
-
-call ant create_dyn_ext_interface_jar
-
-echo ===============================================================================

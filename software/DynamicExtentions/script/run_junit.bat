03312244955de96a171c09b26307a03d2f08413f
sawantg <sawantg>
2011-01-27 14:11:44 +0000

    Merged changes for the latest current working DE branch onto the trunk. The trunk will now be the branch where the latest development code will be considered.
    NOTE - Due the merge the revision history is not available. 
    
    For missing revision history till 1/25/2011, refer to the foll branch -
    http://hjsvn.persistent.co.in/svn/DynamicExtensions/branches/branches_DynamicExtensions/DynamicExtension_1.5.0_31_May_2010
    
    
    
    SVN-Revision: 27288

--- software/DynamicExtentions/script/run_junit.bat
+++ software/DynamicExtentions/script/run_junit.bat
@@ -1,13 +1,13 @@
-cd ./../temp/project
+cd ../temp/project/software/DynamicExtentions
 
 echo ===============================================================================
 echo running for mysql database
 
-copy  "..\..\conf\ApplicationDAOProperties_Mysql.xml" "./src/ApplicationDAOProperties.xml" /Y
+copy  "..\..\..\..\src\conf\ApplicationDAOProperties_Mysql.xml" "./src/java/ApplicationDAOProperties.xml" /Y
 
-copy  "..\..\extra_lib\jta.jar" "./WEB-INF/lib" /Y
+copy  "..\..\..\..\lib\extra\jta.jar" "./lib/server" /Y
 
-copy  "..\..\extra_lib\jboss-j2ee.jar" "./WEB-INF/lib" /Y
+copy  "..\..\..\..\lib\extra\jboss-j2ee.jar" "./lib/server" /Y
 
 call ant db_initialized
 
@@ -15,11 +15,11 @@ call ant generate_schema
 
 call ant generate_codecoverage_report
 
-copy "./query.sql" "..\..\de_release\dynamicExtensions_MySQL.sql" /Y
+copy "./query.sql" "..\..\..\..\binaries\de_release\dynamicExtensions_MySQL.sql" /Y
 
-copy  "..\..\conf\db_oracle.properties" "./db.properties" /Y
+copy  "..\..\..\..\src\conf\db_oracle.properties" "./src/conf/db.properties" /Y
 
 call ant generate_schema -Dis_generate_schema="false"
 
-copy  "./query.sql" "..\..\de_release\dynamicExtensions_Oracle.sql" /Y
+copy  "./query.sql" "..\..\..\..\binaries\de_release\dynamicExtensions_Oracle.sql" /Y
 echo ===============================================================================

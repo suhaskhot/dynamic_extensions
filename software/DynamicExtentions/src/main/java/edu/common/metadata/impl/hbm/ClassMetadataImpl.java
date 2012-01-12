341ffd5b0bea97b7caa65fba464632b4fece8c22
Kunal Kamble <kunal.kamble@krishagni.com>
2012-01-12 10:42:16 +0000

    removed id from the attribute list
    SVN-Revision: 28083

--- software/DynamicExtentions/src/main/java/edu/common/metadata/impl/hbm/ClassMetadataImpl.java
+++ software/DynamicExtentions/src/main/java/edu/common/metadata/impl/hbm/ClassMetadataImpl.java
@@ -81,10 +81,6 @@ public class ClassMetadataImpl implements ClassMetadata {
             	associationMetadata.put(propertyMetadataImpl.getAssociatedClassType(), propertyMetadataImpl);
             }
         }
-        if(idMetadata != null)
-        {
-        	propertiesMetadata.put(idMetadata.getPropertyName(), idMetadata);
-        }
     }
     
     private EntityMetamodel getEntityMetamodel(AbstractEntityPersister persister) {

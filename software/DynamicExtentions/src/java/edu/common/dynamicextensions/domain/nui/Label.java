7af4f7bf61e8701e455672f913bc039a050ed658
Kunal Kamble <kunal.kamble@krishagni.com>
2013-07-25 05:15:49 +0000

    removed extra line spacing between note and field https://cbmiapps.wustl.edu/jira/browse/CLINPORTAL-6942
    SVN-Revision: 29076

--- software/DynamicExtentions/src/java/edu/common/dynamicextensions/domain/nui/Label.java
+++ software/DynamicExtentions/src/java/edu/common/dynamicextensions/domain/nui/Label.java
@@ -55,7 +55,8 @@ public class Label extends Control {
 					.append("</td></tr>");
 		} else if (note) {
 
-			controlHTML.append("<table width='100%'><tr><td width='100%' colspan='3' align='left'>")
+			controlHTML
+					.append("<table width='100%' style='border-collapse: collapse;border-spacing:0;'><tr><td width='100%' colspan='3' align='left'>")
 					.append("<div style='width:100%' class='notes'>")
 					.append(DynamicExtensionsUtility.replaceHTMLSpecialCharacters(getCaption())).append("</div>")
 					.append("</td></tr>");
@@ -70,7 +71,8 @@ public class Label extends Control {
 	@Override
 	protected String attachLabel(String innerHTML) {
 		StringBuilder htmlString = new StringBuilder();
-		htmlString.append("<td colspan='100'>").append(innerHTML).append("</td>");
+		htmlString.append("<td colspan='100' class='formRequiredLabel_withoutBorder'>").append(innerHTML)
+				.append("</td>");
 		return htmlString.toString();
 	}
 
